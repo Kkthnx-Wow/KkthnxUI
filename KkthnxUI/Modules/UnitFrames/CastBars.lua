312610d6ebe5d555392502ab949c8ee92ea46389
Kkthnx <JRussell20@gmail.com>
2016-07-27 22:27:48 -0400

    Cleanup

--- KkthnxUI/Modules/UnitFrames/CastBars.lua
+++ KkthnxUI/Modules/UnitFrames/CastBars.lua
@@ -94,34 +94,23 @@ CastBars:SetScript("OnEvent", function(self, event, addon)
 end)
 
 -- Displays the Casting Bar timer
-CastingBarFrame:HookScript("OnUpdate", function(self, elapsed)
-    if not self.timer then return end
-    if self.update and self.update < elapsed then
-        if self.casting then
-            self.timer:SetText(format("%2.1f / %1.1f", max(self.maxValue - self.value, 0), self.maxValue))
-        elseif self.channeling then
-            self.timer:SetText(format("%.1f", max(self.value, 0)))
-        else
-            self.timer:SetText("")
-        end
-        self.update = 0.1
-    else
-        self.update = self.update - elapsed
-    end
-end)
+local function CastingBarFrame_OnUpdate_Hook(self, elapsed)
+    if(not self.timer) then
+		return
+	end
+	if(self.update) and (self.update < elapsed) then
+		if(self.casting) then
+			self.timer:SetText(format("%2.1f / %1.1f", max(self.maxValue - self.value, 0), self.maxValue))
+		elseif(self.channeling) then
+			self.timer:SetText(format("%.1f", max(self.value, 0)))
+		else
+			self.timer:SetText("")
+		end
+		self.update = 0.1
+	else
+		self.update = self.update - elapsed
+	end
+end
 
-TargetFrameSpellBar:HookScript("OnUpdate", function(self, elapsed)
-    if not self.timer then return end
-    if self.update and self.update < elapsed then
-        if self.casting then
-            self.timer:SetText(format("%2.1f / %1.1f", max(self.maxValue - self.value, 0), self.maxValue))
-        elseif self.channeling then
-            self.timer:SetText(format("%.1f", max(self.value, 0)))
-        else
-            self.timer:SetText("")
-        end
-        self.update = 0.1
-    else
-        self.update = self.update - elapsed
-    end
-end)
+CastingBarFrame:HookScript("OnUpdate", CastingBarFrame_OnUpdate_Hook)
+TargetFrameSpellBar:HookScript("OnUpdate", CastingBarFrame_OnUpdate_Hook)
