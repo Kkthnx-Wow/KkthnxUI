24d0ee6120958e39cd33ecb57d1f42afc6e06709
Kkthnx <JRussell20@gmail.com>
2016-08-17 18:27:08 -0400

    FIxed Interrupts.

--- KkthnxUI/Modules/Announcements/Interrupt.lua
+++ KkthnxUI/Modules/Announcements/Interrupt.lua
@@ -1,16 +1,15 @@
 local K, C, L, _ = select(2, ...):unpack()
 if C.Announcements.Interrupt ~= true then return end
 
-local GetSpellLink = GetSpellLink
-local CreateFrame = CreateFrame
+local format = string.format
 local SendChatMessage = SendChatMessage
 
 -- ANNOUNCE YOUR INTERRUPTS
-local Interrupt = CreateFrame("Frame")
-Interrupt:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-Interrupt:SetScript("OnEvent", function(self, _, ...)
-	local _, event, _, sourceGUID, _, _, _, _, destName, _, _, spellID = ...
+local Interrupts = CreateFrame("Frame")
+Interrupts:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+Interrupts:SetScript("OnEvent", function(self, _, ...)
+	local _, event, _, sourceGUID, _, _, _, _, destName, _, _, _, _, _, spellID, spellName = ...
 	if not (event == "SPELL_INTERRUPT" and (sourceGUID == UnitGUID("player") or sourceGUID == UnitGUID("pet"))) then return end
 
-	SendChatMessage(L_ANNOUNCE_INTERRUPTED.." "..destName..": "..GetSpellLink(spellID), K.CheckChat())
+	SendChatMessage(format(L_ANNOUNCE_INTERRUPTED, destName, spellID, spellName), K.CheckChat())
 end)
