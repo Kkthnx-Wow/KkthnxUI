d74e3ce870677111d3a689a398aa5ef0c594d592
Kkthnx <JRussell20@gmail.com>
2016-08-17 21:22:13 -0400

    All new code for Unitframes.

--- KkthnxUI/Modules/UnitFrames/EnhancedFrames.lua
+++ KkthnxUI/Modules/UnitFrames/EnhancedFrames.lua
@@ -10,6 +10,8 @@ local CreateFrame = CreateFrame
 local hooksecurefunc = hooksecurefunc
 local InCombatLockdown = InCombatLockdown
 local MAX_PARTY_MEMBERS = MAX_PARTY_MEMBERS
+local EnhancedFrames = CreateFrame("Frame")
+local EnhancedPartyFrames = CreateFrame("Frame")
 
 local shorts = {
 	{ 1e10, 1e9, "%.0fB" }, -- 10b+ as 12B
@@ -24,60 +26,93 @@ for i = 1, #shorts do
 	shorts[i][4] = shorts[i][3] .. " (%.0f%%)"
 end
 
-if (PlayerFrame) then
+-- Event listener to make sure we enable the addon at the right time
+function EnhancedFrames:PLAYER_ENTERING_WORLD()
+
+	EnableEnhancedFrames()
+	--EnableEnhancedPartyFrames()
+end
+
+function EnableEnhancedFrames()
+	-- GENERIC STATUS TEXT HOOK
+	hooksecurefunc("TextStatusBar_UpdateTextStringWithValues", EnhancedFrames_UpdateTextStringWithValues)
+
+	-- HOOK PLAYERFRAME FUNCTIONS
+	hooksecurefunc("PlayerFrame_ToPlayerArt", EnhancedFrames_PlayerFrame_ToPlayerArt)
+	hooksecurefunc("PlayerFrame_ToVehicleArt", EnhancedFrames_PlayerFrame_ToVehicleArt)
+
+	-- HOOK TARGETFRAME FUNCTIONS
+	hooksecurefunc("TargetFrame_CheckDead", EnhancedFrames_TargetFrame_Update)
+	hooksecurefunc("TargetFrame_Update", EnhancedFrames_TargetFrame_Update)
+	hooksecurefunc("TargetFrame_CheckFaction", EnhancedFrames_TargetFrame_CheckFaction)
+	hooksecurefunc("TargetFrame_CheckClassification", EnhancedFrames_Target_Classification)
+	hooksecurefunc("TargetofTarget_Update", EnhancedFrames_TargetFrame_Update)
+
+	-- BOSSFRAME HOOKS
+	hooksecurefunc("BossTargetFrame_OnLoad", EnhancedFrames_BossTargetFrame_Style)
+	
+	hooksecurefunc("PartyMemberFrame_ToPlayerArt", EnhancedPartyFrames_PartyMemberFrame_ToPlayerArt)
+	hooksecurefunc("PartyMemberFrame_ToVehicleArt", EnhancedPartyFrames_PartyMemberFrame_ToVehicleArt)
+
+	-- SET UP SOME STYLINGS
+	EnhancedFrames_Style_PlayerFrame()
+	EnhancedFrames_BossTargetFrame_Style(Boss1TargetFrame)
+	EnhancedFrames_BossTargetFrame_Style(Boss2TargetFrame)
+	EnhancedFrames_BossTargetFrame_Style(Boss3TargetFrame)
+	EnhancedFrames_BossTargetFrame_Style(Boss4TargetFrame)
+	EnhancedFrames_Style_TargetFrame(TargetFrame)
+	EnhancedFrames_Style_TargetFrame(FocusFrame)
+
+	-- UPDATE SOME VALUES
+	TextStatusBar_UpdateTextString(PlayerFrame.healthbar)
+	TextStatusBar_UpdateTextString(PlayerFrame.manabar)
+end
+
+function EnhancedFrames_Style_PlayerFrame()
+	if not InCombatLockdown() then
+		PlayerName:SetWidth(0.01)
+
+		PlayerFrameHealthBar.capNumericDisplay = true
+		PlayerFrameHealthBar:SetWidth(119)
+		PlayerFrameHealthBar:SetHeight(29)
+		PlayerFrameHealthBar:SetPoint("TOPLEFT", 106, -22)
+		PlayerFrameHealthBarText:SetPoint("CENTER", 50, 12)
+	end
+
 	PlayerFrameTexture:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\UI-TargetingFrame")
 	PlayerStatusTexture:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\UI-Player-Status")
 end
 
-hooksecurefunc("TargetFrame_CheckClassification", function (self, forceNormalTexture)
-	--if(InCombatLockdown() == false) then
-		local Classification = UnitClassification(self.unit)
+function EnhancedFrames_Style_TargetFrame(self)
+	local classification = UnitClassification(self.unit)
+	if (classification == "minus") then
+		self.healthbar:SetHeight(12)
+		self.healthbar:SetPoint("TOPLEFT", 7, -41)
+		self.healthbar.TextString:SetPoint("CENTER", -50, 4)
+		self.deadText:SetPoint("CENTER", -50, 4)
+		self.Background:SetPoint("TOPLEFT", 7, -41)
+	else
+		self.name:ClearAllPoints()
+		self.name:SetPoint("CENTER", TargetFrame, "CENTER", -50, 35)
 
-		if forceNormalTexture then
-			self.borderTexture:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\UI-TargetingFrame")
-		elseif Classification == "minus" then
-			self.borderTexture:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\UI-Targeting-MinusFrame")
-			self.nameBackground:Hide()
-			self.manabar:Hide()
-			self.manabar.TextString:Hide()
-			forceNormalTexture = true
-		elseif Classification == "worldboss" or Classification == "elite" then
-			self.borderTexture:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\UI-TargetingFrame-Elite")
-		elseif Classification == "rareelite" then
-			self.borderTexture:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\UI-TargetingFrame-Rare-Elite")
-		elseif Classification == "rare" then
-			self.borderTexture:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\UI-TargetingFrame-Rare")
-		else
-			self.borderTexture:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\UI-TargetingFrame")
-			forceNormalTexture = true
-		end
-	--end
-end)
-
-for i = 1, MAX_PARTY_MEMBERS do
-	if(InCombatLockdown() == false) then
-		_G["PartyMemberFrame"..i.."Texture"]:SetSize(128, 64)
-		_G["PartyMemberFrame"..i.."Texture"]:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\UI-PartyFrame")
-
-		_G["PartyMemberFrame"..i.."HealthBar"]:SetSize(69, 12)
-		_G["PartyMemberFrame"..i.."HealthBar"]:ClearAllPoints()
-		_G["PartyMemberFrame"..i.."HealthBar"]:SetPoint("CENTER", 17, 7)
-
-		_G["PartyMemberFrame"..i.."ManaBar"]:SetSize(70, 7)
-		_G["PartyMemberFrame"..i.."ManaBar"]:ClearAllPoints()
-		_G["PartyMemberFrame"..i.."ManaBar"]:SetPoint("CENTER", 17, -2)
-
-		_G["PartyMemberFrame"..i.."HealthBarText"]:ClearAllPoints()
-		_G["PartyMemberFrame"..i.."HealthBarText"]:SetPoint("CENTER", _G["PartyMemberFrame"..i.."HealthBar"], "CENTER", 0, 1)
-
-		_G["PartyMemberFrame"..i.."ManaBarText"]:ClearAllPoints()
-		_G["PartyMemberFrame"..i.."ManaBarText"]:SetPoint("CENTER", _G["PartyMemberFrame"..i.."ManaBar"], "CENTER", 0, 0)
-
-		_G["PartyMemberFrame"..i.."Flash"]:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\Party-Flash")
+		self.healthbar:SetHeight(29)
+		self.healthbar:SetPoint("TOPLEFT", 7, -22)
+		self.healthbar.TextString:SetPoint("CENTER", -50, 12)
+		self.deadText:SetPoint("CENTER", -50, 12)
+		self.nameBackground:Hide()
+		self.Background:SetPoint("TOPLEFT", 7, -22)
 	end
+
+	self.healthbar:SetWidth(119)
 end
 
-hooksecurefunc("TextStatusBar_UpdateTextStringWithValues", function(statusBar, textString, value, valueMin, valueMax)
+function EnhancedFrames_BossTargetFrame_Style(self)
+	self.borderTexture:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\UI-UnitFrame-Boss")
+
+	EnhancedFrames_Style_TargetFrame(self)
+end
+
+function EnhancedFrames_UpdateTextStringWithValues(statusFrame, textString, value, valueMin, valueMax)
 	if value == 0 then
 		return textString:SetText("")
 	end
@@ -104,108 +139,168 @@ hooksecurefunc("TextStatusBar_UpdateTextStringWithValues", function(statusBar, t
 			end
 		end
 	end
-end)
-
-if(InCombatLockdown() == false) then
-	FocusFrameToT:ClearAllPoints()
-	FocusFrameToT:SetPoint("CENTER", FocusFrame, "CENTER", 60, -45)
-
-	-- NAMES
-	PlayerName:SetWidth(0.01)
-
-	TargetFrame.name:ClearAllPoints()
-	TargetFrame.name:SetPoint("CENTER", TargetFrame, "CENTER", -50, 35)
-	TargetFrame.name.SetPoint = K.Noop
-
-	FocusFrame.name:ClearAllPoints()
-	FocusFrame.name:SetPoint("CENTER", FocusFrame, "CENTER", -45, 35)
-	FocusFrame.name.SetPoint = K.Noop
-
-	-- PLAYER BARS
-	PlayerFrameHealthBar:SetHeight(27)
-	PlayerFrameHealthBar:ClearAllPoints()
-	PlayerFrameHealthBar:SetPoint("CENTER", PlayerFrame, "CENTER", 50, 14)
-	PlayerFrameHealthBar.SetPoint = K.Noop
-
-	PlayerFrameManaBar:ClearAllPoints()
-	PlayerFrameManaBar:SetPoint("CENTER", PlayerFrame, "CENTER", 51, -7)
-	PlayerFrameManaBar.SetPoint = K.Noop
-
-	-- TARGET BARS
-	TargetFrameHealthBar:SetHeight(27)
-	TargetFrameHealthBar:ClearAllPoints()
-	TargetFrameHealthBar:SetPoint("CENTER", TargetFrame, "CENTER", -50, 14)
-	TargetFrameHealthBar.SetPoint = K.Noop
-
-	TargetFrameTextureFrameDeadText:ClearAllPoints()
-	TargetFrameTextureFrameDeadText:SetPoint("CENTER", TargetFrameHealthBar, "CENTER", 0, 0)
-	TargetFrameTextureFrameDeadText.SetPoint = K.Noop
-
-	TargetFrameManaBar:ClearAllPoints()
-	TargetFrameManaBar:SetPoint("CENTER", TargetFrame, "CENTER", -51, -7)
-	TargetFrameManaBar.SetPoint = K.Noop
-
-	TargetFrameNumericalThreat:SetScale(0.9)
-	TargetFrameNumericalThreat:ClearAllPoints()
-	TargetFrameNumericalThreat:SetPoint("BOTTOM", PlayerFrame, "TOP", 75, -23)
-	TargetFrameNumericalThreat.SetPoint = K.Noop
-
-	-- FOCUS BARS
-	FocusFrameHealthBar:SetHeight(27)
-	FocusFrameHealthBar:ClearAllPoints()
-	FocusFrameHealthBar:SetPoint("CENTER", FocusFrame, "CENTER", -50, 14)
-	FocusFrameHealthBar.SetPoint = K.Noop
-
-	FocusFrameTextureFrameDeadText:ClearAllPoints()
-	FocusFrameTextureFrameDeadText:SetPoint("CENTER", FocusFrameHealthBar, "CENTER", 0, 0)
-	FocusFrameTextureFrameDeadText.SetPoint = K.Noop
-
-	FocusFrameManaBar:ClearAllPoints()
-	FocusFrameManaBar:SetPoint("CENTER", FocusFrame, "CENTER", -51, -7)
-	FocusFrameManaBar.SetPoint = K.Noop
-
-	FocusFrameNumericalThreat:ClearAllPoints()
-	FocusFrameNumericalThreat:SetPoint("CENTER", FocusFrame, "CENTER", 44, 48)
-	FocusFrameNumericalThreat.SetPoint = K.Noop
-
-	-- TEXTSTRINGS
-	TargetFrameHealthBar.TextString:ClearAllPoints()
-	TargetFrameHealthBar.TextString:SetPoint("CENTER", TargetFrame, "CENTER", -53, 12)
-	TargetFrameHealthBar.TextString.SetPoint = K.Noop
-
-	PlayerFrameHealthBar.TextString:ClearAllPoints()
-	PlayerFrameHealthBar.TextString:SetPoint("CENTER", PlayerFrame, "CENTER", 53, 12)
-	PlayerFrameHealthBar.TextString.SetPoint = K.Noop
-
-	PlayerFrameHealthBarTextRight:ClearAllPoints()
-	PlayerFrameHealthBarTextRight:SetPoint("RIGHT", PlayerFrame, "RIGHT", -8, 12)
-	PlayerFrameHealthBarTextRight.SetPoint = K.Noop
-
-	PlayerFrameHealthBarTextLeft:ClearAllPoints()
-	PlayerFrameHealthBarTextLeft:SetPoint("CENTER", PlayerFrame, "CENTER", 8, 12)
-	PlayerFrameHealthBarTextLeft.SetPoint = K.Noop
-
-	TargetFrameTextureFrameHealthBarTextRight:ClearAllPoints()
-	TargetFrameTextureFrameHealthBarTextRight:SetPoint("CENTER", TargetFrame, "CENTER", -13, 12)
-	TargetFrameTextureFrameHealthBarTextRight.SetPoint = K.Noop
-
-	TargetFrameTextureFrameHealthBarTextLeft:ClearAllPoints()
-	TargetFrameTextureFrameHealthBarTextLeft:SetPoint("LEFT", TargetFrame, "LEFT", 7, 12)
-	TargetFrameTextureFrameHealthBarTextLeft.SetPoint = K.Noop
-
-	FocusFrameHealthBar.TextString:ClearAllPoints()
-	FocusFrameHealthBar.TextString:SetPoint("CENTER", FocusFrame, "CENTER", -53, 12)
-	FocusFrameHealthBar.TextString.SetPoint = K.Noop
-
-	PlayerFrameManaBar.TextString:ClearAllPoints()
-	PlayerFrameManaBar.TextString:SetPoint("CENTER", PlayerFrame, "CENTER", 53, -7)
-	PlayerFrameManaBar.TextString.SetPoint = K.Noop
-
-	TargetFrameManaBar.TextString:ClearAllPoints()
-	TargetFrameManaBar.TextString:SetPoint("CENTER", TargetFrame, "CENTER", -53, -7)
-	TargetFrameManaBar.TextString.SetPoint = K.Noop
-
-	FocusFrameManaBar.TextString:ClearAllPoints()
-	FocusFrameManaBar.TextString:SetPoint("CENTER", FocusFrame, "CENTER", -53, -7)
-	FocusFrameManaBar.TextString.SetPoint = K.Noop
 end
+
+function EnhancedFrames_PlayerFrame_ToPlayerArt(self)
+	if not InCombatLockdown() then
+		EnhancedFrames_Style_PlayerFrame()
+	end
+end
+
+function EnhancedFrames_PlayerFrame_ToVehicleArt(self)
+	if not InCombatLockdown() then
+		PlayerFrameHealthBar:SetHeight(12)
+		PlayerFrameHealthBarText:SetPoint("CENTER", 50, 3)
+	end
+end
+
+function EnhancedFrames_TargetFrame_Update(self)
+	-- Set back color of health bar
+	if (not UnitPlayerControlled(self.unit) and UnitIsTapDenied(self.unit)) then
+		-- Gray if npc is tapped by other player
+		self.healthbar:SetStatusBarColor(0.5, 0.5, 0.5)
+	end
+end
+
+function EnhancedFrames_Target_Classification(self, forceNormalTexture)
+	local texture
+	local classification = UnitClassification(self.unit)
+	if (classification == "worldboss" or classification == "elite") then
+		texture = "Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\UI-TargetingFrame-Elite"
+	elseif (classification == "rareelite") then
+		texture = "Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\UI-TargetingFrame-Rare-Elite"
+	elseif (classification == "rare") then
+		texture = "Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\UI-TargetingFrame-Rare"
+	end
+	if (texture and not forceNormalTexture) then
+		self.borderTexture:SetTexture(texture)
+	else
+		if (not (classification == "minus")) then
+			self.borderTexture:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\UI-TargetingFrame")
+		end
+	end
+
+	self.nameBackground:Hide()
+end
+
+function EnhancedFrames_TargetFrame_CheckFaction(self)
+	local factionGroup = UnitFactionGroup(self.unit)
+	if (UnitIsPVPFreeForAll(self.unit)) then
+		self.pvpIcon:SetTexture("Interface\\TargetingFrame\\UI-PVP-FFA")
+		self.pvpIcon:Show()
+	elseif (factionGroup and UnitIsPVP(self.unit) and UnitIsEnemy("player", self.unit)) then
+		self.pvpIcon:SetTexture("Interface\\TargetingFrame\\UI-PVP-FFA")
+		self.pvpIcon:Show()
+	elseif (factionGroup == "Alliance" or factionGroup == "Horde") then
+		self.pvpIcon:SetTexture("Interface\\TargetingFrame\\UI-PVP-"..factionGroup)
+		self.pvpIcon:Show()
+	else
+		self.pvpIcon:Hide()
+	end
+
+	EnhancedFrames_Style_TargetFrame(self)
+end
+
+-- STYLE - PARTY MEMEBER FRAME STYLE CHANGES
+function EnhancedPartyFrames_PartyMemberFrame_ToPlayerArt(self)
+	if not InCombatLockdown() then
+		for i = 1, MAX_PARTY_MEMBERS do
+			_G["PartyMemberFrame"..i.."HealthBarText"]:SetPoint("CENTER", _G["PartyMemberFrame"..i.."HealthBar"], "CENTER", 0, 1)
+
+			_G["PartyMemberFrame"..i.."Name"]:SetPoint("TOP", 0, 20)
+			_G["PartyMemberFrame"..i.."Name"]:SetFont(C.Media.Font, 10)
+
+			_G["PartyMemberFrame"..i.."Texture"]:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\PartyFrame")
+			_G["PartyMemberFrame"..i.."Texture"]:SetPoint("TOPLEFT", 0, 6)
+
+			_G["PartyMemberFrame"..i.."Flash"]:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\PartyFrameFlash")
+			_G["PartyMemberFrame"..i.."Flash"]:SetPoint("TOPLEFT", 0, 6)
+
+			_G["PartyMemberFrame"..i.."HealthBar"]:SetPoint("TOPLEFT", 47, -3)
+			_G["PartyMemberFrame"..i.."HealthBar"]:SetHeight(17)
+
+			_G["PartyMemberFrame"..i.."Background"]:SetPoint("TOPLEFT", 46, -3)
+			_G["PartyMemberFrame"..i.."Background"]:SetSize(70, 24)
+			_G["PartyMemberFrame"..i.."Background"]:SetPoint("TOPLEFT", 47, -3)
+		end
+	end
+end
+
+-- UPDATE SETTINGS SPECIFIC TO PARTY MEMBER UNIT FRAMES WHEN IN VEHICLES
+function EnhancedPartyFrames_PartyMemberFrame_ToVehicleArt(self)
+	if not InCombatLockdown() then
+		PartyMemberFrame1VehicleTexture:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\VehiclePartyFrame")
+		PartyMemberFrame2VehicleTexture:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\VehiclePartyFrame")
+		PartyMemberFrame3VehicleTexture:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\VehiclePartyFrame")
+		PartyMemberFrame4VehicleTexture:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\VehiclePartyFrame")
+	end
+end
+
+-- BOOTSTRAP
+function EnhancedFrames_StartUp(self)
+	self:SetScript("OnEvent", function(self, event) self[event](self) end)
+	self:RegisterEvent("PLAYER_ENTERING_WORLD")
+end
+
+EnhancedFrames_StartUp(EnhancedFrames)
+--EnhancedPartyFrames_StartUp(EnhancedPartyFrames)
+
+--[[
+----------------------------------------------------------------------
+-- HERE TO START OUR PARTYFRAMES. WE SHOULD MERGE ALL THIS CODE LATER.
+----------------------------------------------------------------------
+
+-- EVENT LISTENER TO MAKE SURE WE ENABLE THE ADDON AT THE RIGHT TIME
+function EnhancedPartyFrames:PLAYER_ENTERING_WORLD()
+	EnableEnhancedPartyFrames()
+end
+
+-- HOOKING GAME FUNCTIONS AND SETTING THEM TO FIRE ADDON FUNCTION
+function EnableEnhancedPartyFrames()
+	hooksecurefunc("PartyMemberFrame_ToPlayerArt", EnhancedPartyFrames_PartyMemberFrame_ToPlayerArt)
+	hooksecurefunc("PartyMemberFrame_ToVehicleArt", EnhancedPartyFrames_PartyMemberFrame_ToVehicleArt)
+end
+
+-- STYLE - PARTY MEMEBER FRAME STYLE CHANGES
+function EnhancedPartyFrames_PartyMemberFrame_ToPlayerArt(self)
+	if not InCombatLockdown() then
+		for i = 1, MAX_PARTY_MEMBERS do
+			_G["PartyMemberFrame"..i.."HealthBarText"]:SetPoint("CENTER", _G["PartyMemberFrame"..i.."HealthBar"], "CENTER", 0, 1)
+
+			_G["PartyMemberFrame"..i.."Name"]:SetPoint("TOP", 0, 20)
+			_G["PartyMemberFrame"..i.."Name"]:SetFont(C.Media.Font, 10)
+
+			_G["PartyMemberFrame"..i.."Texture"]:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\PartyFrame")
+			_G["PartyMemberFrame"..i.."Texture"]:SetPoint("TOPLEFT", 0, 6)
+
+			_G["PartyMemberFrame"..i.."Flash"]:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\PartyFrameFlash")
+			_G["PartyMemberFrame"..i.."Flash"]:SetPoint("TOPLEFT", 0, 6)
+
+			_G["PartyMemberFrame"..i.."HealthBar"]:SetPoint("TOPLEFT", 47, -3)
+			_G["PartyMemberFrame"..i.."HealthBar"]:SetHeight(17)
+
+			_G["PartyMemberFrame"..i.."Background"]:SetPoint("TOPLEFT", 46, -3)
+			_G["PartyMemberFrame"..i.."Background"]:SetSize(70, 24)
+			_G["PartyMemberFrame"..i.."Background"]:SetPoint("TOPLEFT", 47, -3)
+		end
+	end
+end
+
+-- UPDATE SETTINGS SPECIFIC TO PARTY MEMBER UNIT FRAMES WHEN IN VEHICLES
+function EnhancedPartyFrames_PartyMemberFrame_ToVehicleArt(self)
+	if not InCombatLockdown() then
+		PartyMemberFrame1VehicleTexture:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\VehiclePartyFrame")
+		PartyMemberFrame2VehicleTexture:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\VehiclePartyFrame")
+		PartyMemberFrame3VehicleTexture:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\VehiclePartyFrame")
+		PartyMemberFrame4VehicleTexture:SetTexture("Interface\\Addons\\KkthnxUI\\Media\\Unitframes\\VehiclePartyFrame")
+	end
+end
+
+-- BOOTSTRAP
+function EnhancedPartyFrames_StartUp(self)
+	self:SetScript("OnEvent", function(self, event) self[event](self) end)
+	self:RegisterEvent("PLAYER_ENTERING_WORLD")
+end
+
+EnhancedPartyFrames_StartUp(EnhancedPartyFrames)
+]]--
