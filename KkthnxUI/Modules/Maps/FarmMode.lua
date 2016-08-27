6ddfef75e97ba37fc451c50943b4da47bca96c7d
Kkthnx <JRussell20@gmail.com>
2016-08-27 04:03:50 -0400

    Update.
    Update Farm-Mode
    Type /fm /farmmode or hover to the left of the datatext module

--- KkthnxUI/Modules/Maps/FarmMode.lua
+++ KkthnxUI/Modules/Maps/FarmMode.lua
@@ -1,25 +1,43 @@
 local K, C, L, _ = select(2, ...):unpack()
 if C.Minimap.Enable ~= true then return end
 
--- Minimap Farmmode
+-- FARM-MODE
 local Farm = false
-local MiniSize = 150
-local FarmSize = 230
-function SlashCmdList.FARMMODE(msg, editbox)
+SlashCmdList.FARMMODE = function()
 	if Farm == false then
-		if InCombatLockdown() then
-			K.Print("|cffffe02e"..ERR_NOT_IN_COMBAT.."|r") return
-		end
-		Minimap:SetSize(FarmSize, FarmSize)
-		MinimapAnchor:SetSize(FarmSize, FarmSize)
+		MinimapAnchor:SetSize(C.Minimap.Size * 1.65, C.Minimap.Size * 1.65)
+		Minimap:SetSize(MinimapAnchor:GetWidth(), MinimapAnchor:GetWidth())
 		Farm = true
-		K.Print(L_MINIMAP_FARMMODE_ON)
 	else
-		Minimap:SetSize(MiniSize, MiniSize)
-		MinimapAnchor:SetSize(MiniSize, MiniSize)
+		MinimapAnchor:SetSize(C.Minimap.Size, C.Minimap.Size)
+		Minimap:SetSize(MinimapAnchor:GetWidth(), MinimapAnchor:GetWidth())
 		Farm = false
-		K.Print(L_MINIMAP_FARMMODE_OFF)
 	end
 end
 SLASH_FARMMODE1 = "/farmmode"
 SLASH_FARMMODE2 = "/fm"
+
+-- BUTTON FOR FARM-MODE
+local Farm = CreateFrame("Button", "FarmMode", UIParent)
+K.CreateBorder(Farm, 10)
+Farm:SetPoint("TOP", ToggleBar5, "BOTTOM", 0, -4)
+Farm:SetSize(19, 19)
+Farm:SetAlpha(0)
+
+Farm.t = Farm:CreateTexture(nil, "OVERLAY")
+Farm.t:SetTexture("Interface\\Icons\\inv_misc_map_01")
+Farm.t:SetTexCoord(unpack(K.TexCoords))
+Farm.t:SetPoint("TOPLEFT", Farm, 2, -2)
+Farm.t:SetPoint("BOTTOMRIGHT", Farm, -2, 2)
+
+Farm:SetScript("OnClick", function()
+	SlashCmdList.FARMMODE()
+end)
+
+Farm:SetScript("OnEnter", function()
+	Farm:FadeIn()
+end)
+
+Farm:SetScript("OnLeave", function()
+	Farm:FadeOut()
+end)
