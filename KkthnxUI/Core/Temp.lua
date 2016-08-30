af6dca0a9b52497471890cf60b2e5b7210441542
Kkthnx <JRussell20@gmail.com>
2016-08-30 06:49:40 -0400

    Updates.
    Skinned orderhall bar.
    Redid exp/rep/artifact bars
    Misc.
    
    I will push more in a bit. I havent even really leveled yet.

--- KkthnxUI/Core/Temp.lua
+++ KkthnxUI/Core/Temp.lua
@@ -2,3 +2,104 @@ local K, C, L, _ = select(2, ...):unpack()
 
 -- THIS FILE IS FOR TESTING AND REMINDERS BULLSHIT :D
 -- [[ -*- NOTES -*- ]] --
+
+-- ARTIFACTBAR LOCALIZATION
+L_ARTIFACTBAR_XPTITLE = "Artifact Experience"
+L_ARTIFACTBAR_CURRENTXP = "Current Experience: %s"
+L_ARTIFACTBAR_XP = "Experience: %s/%s (%d%%)"
+L_ARTIFACTBAR_XPREMAINING = "Remaining: %s"
+L_ARTIFACTBAR_TRAITS = "Traits avaiable: %s"
+
+local BarHeight, BarWidth = 8, 150
+local Texture = C.Media.Texture
+local Color = RAID_CLASS_COLORS[K.Class]
+
+local Artifact_Backdrop = CreateFrame("Frame", "Artifact_Backdrop", UIParent)
+Artifact_Backdrop:SetSize(BarWidth, BarHeight)
+Artifact_Backdrop:SetPoint("TOPLEFT", Minimap, "BOTTOMLEFT", -1, -40)
+Artifact_Backdrop:SetPoint("TOPRIGHT", Minimap, "BOTTOMRIGHT", 1, -40)
+Artifact_Backdrop:SetBackdropColor(C.Media.Backdrop_Color)
+Artifact_Backdrop:SetBackdropBorderColor(C.Media.Backdrop_Color)
+Artifact_Backdrop:SetFrameStrata("LOW")
+Artifact_Backdrop:CreatePixelShadow()
+
+local Artifact_BackdropBG = CreateFrame("Frame", "StatusBarBG", Artifact_Backdrop)
+Artifact_BackdropBG:SetFrameLevel(Artifact_Backdrop:GetFrameLevel() - 1)
+Artifact_BackdropBG:SetPoint("TOPLEFT", -1, 1)
+Artifact_BackdropBG:SetPoint("BOTTOMRIGHT", 1, -1)
+Artifact_BackdropBG:SetBackdrop(K.BorderBackdrop)
+Artifact_BackdropBG:SetBackdropColor(unpack(C.Media.Backdrop_Color))
+
+local ArtifactBar = CreateFrame("StatusBar", "XP_ArtifactBar", Artifact_Backdrop, "TextStatusBar")
+ArtifactBar:SetSize(BarWidth, BarHeight)
+ArtifactBar:SetPoint("TOP", Artifact_Backdrop, "TOP", 0, 0)
+ArtifactBar:SetStatusBarTexture(Texture)
+ArtifactBar:SetStatusBarColor(157/255, 138/255, 108/255)
+
+local ArtifactMouseFrame = CreateFrame("Frame", "Artifact_MouseFrame", Artifact_Backdrop)
+ArtifactMouseFrame:SetAllPoints(Artifact_Backdrop)
+ArtifactMouseFrame:EnableMouse(true)
+ArtifactMouseFrame:SetFrameLevel(3)
+
+local function updateStatus()
+	local HasArtBar = HasArtifactEquipped()
+
+	if HasArtBar then
+		local _, _, _, _, totalxp, pointsSpent, _, _, _, _, _, _ = C_ArtifactUI.GetEquippedArtifactInfo()
+		local _, xp, xpForNextPoint = MainMenuBar_GetNumArtifactTraitsPurchasableFromXP(pointsSpent, totalxp)
+
+		Artifact_Backdrop:Show()
+		ArtifactBar:SetMinMaxValues(min(0, xp), xpForNextPoint)
+		ArtifactBar:SetValue(xp)
+	else
+		Artifact_Backdrop:Hide()
+	end
+
+	ArtifactMouseFrame:SetScript("OnEnter", function()
+		GameTooltip:SetOwner(ArtifactMouseFrame, "ANCHOR_TOPRIGHT", 2, 5)
+		GameTooltip:ClearLines()
+		if HasArtBar then
+			local _, _, _, _, totalxp, pointsSpent, _, _, _, _, _, _ = C_ArtifactUI.GetEquippedArtifactInfo()
+			local numPointsAvailableToSpend, xp, xpForNextPoint = MainMenuBar_GetNumArtifactTraitsPurchasableFromXP(pointsSpent, totalxp)
+
+			GameTooltip:AddLine(L_ARTIFACTBAR_XPTITLE)
+			GameTooltip:AddLine(format(L_ARTIFACTBAR_XP, xp, xpForNextPoint, (xp / xpForNextPoint) * 100))
+			GameTooltip:AddLine(format(L_ARTIFACTBAR_XPREMAINING, xpForNextPoint - xp))
+			GameTooltip:AddLine(format(L_ARTIFACTBAR_TRAITS, numPointsAvailableToSpend))
+		end
+
+		GameTooltip:Show()
+	end)
+
+	ArtifactMouseFrame:SetScript("OnLeave", function() GameTooltip:Hide() end)
+end
+
+local Frame = CreateFrame("Frame", nil, UIParent)
+Frame:RegisterEvent("ARTIFACT_XP_UPDATE")
+Frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+Frame:RegisterEvent("UNIT_INVENTORY_CHANGED")
+Frame:SetScript("OnEvent", updateStatus)
+
+
+local OHSkin = CreateFrame("Frame")
+OHSkin:RegisterEvent("ADDON_LOADED")
+OHSkin:SetScript("OnEvent", function(self, event, addon)
+	if (addon ~= "Blizzard_OrderHallUI") then
+		return
+	end
+
+	OrderHallCommandBar:StripTextures()
+	OrderHallCommandBar:SetTemplate("Transparent")
+	OrderHallCommandBar:ClearAllPoints()
+	OrderHallCommandBar:SetPoint("TOP", UIParent, 0, 0)
+	OrderHallCommandBar:SetWidth(480)
+	OrderHallCommandBar.ClassIcon:SetTexture("Interface\\TargetingFrame\\UI-Classes-Circles")
+	OrderHallCommandBar.ClassIcon:SetSize(46, 20)
+	OrderHallCommandBar.CurrencyIcon:SetAtlas("legionmission-icon-currency", false)
+	OrderHallCommandBar.AreaName:ClearAllPoints()
+	OrderHallCommandBar.AreaName:SetPoint("RIGHT", OrderHallCommandBar, "RIGHT", -10, 0)
+	OrderHallCommandBar.WorldMapButton:Hide()
+	--OrderHallCommandBar.WorldMapButton:SetPoint("RIGHT", OrderHallCommandBar, -5, -2)
+
+	self:UnregisterEvent("ADDON_LOADED")
+end)
--- KkthnxUI/Modules/Blizzard/Experience.lua
+++ KkthnxUI/Modules/Blizzard/Experience.lua
@@ -1,224 +1,144 @@
 local K, C, L, _ = select(2, ...):unpack()
-if C.ExpRep.ExpEnable ~= true then return end
 
-local Experience = CreateFrame("Frame", nil, UIParent)
-local HideTooltip = GameTooltip_Hide
-local Bars = 20
+local barHeight, barWidth = 8, 150
+local barTex, flatTex = C.Media.Texture
+local color = RAID_CLASS_COLORS[K.Class]
 
-Experience.NumBars = 2
-Experience.RestedColor = {75/255, 175/255, 76/255}
-Experience.XPColor = {0/255, 144/255, 255/255}
-Experience.AFColor = {229/255, 204/255, 127/255}
-Experience.HNColor = {222/255, 22/255, 22/255}
+local FactionInfo = {
+	[1] = {{170/255, 70/255, 70/255}, L_REPUTATION_HATED, "FFaa4646"},
+	[2] = {{170/255, 70/255, 70/255}, L_REPUTATION_HOSTILE, "FFaa4646"},
+	[3] = {{170/255, 70/255, 70/255}, L_REPUTATION_UNFRIENDLY, "FFaa4646"},
+	[4] = {{200/255, 180/255, 100/255}, L_REPUTATION_NEUTRAL, "FFc8b464"},
+	[5] = {{75/255, 175/255, 75/255}, L_REPUTATION_FRIENDLY, "FF4baf4b"},
+	[6] = {{75/255, 175/255, 75/255}, L_REPUTATION_HONORED, "FF4baf4b"},
+	[7] = {{75/255, 175/255, 75/255}, L_REPUTATION_REVERED, "FF4baf4b"},
+	[8] = {{155/255, 255/255, 155/255}, L_REPUTATION_EXALTED,"FF9bff9b"},
+}
 
-local ExperienceAnchor = CreateFrame("Frame", "ExperienceAnchor", UIParent)
-ExperienceAnchor:SetSize(C.ExpRep.ExpWidth, 18)
-ExperienceAnchor:SetPoint("TOPLEFT", Minimap, "BOTTOMLEFT", -1, -22)
-ExperienceAnchor:SetPoint("TOPRIGHT", Minimap, "BOTTOMRIGHT", 1, -22)
+function colorize(r) return FactionInfo[r][3] end
 
-function Experience:SetTooltip()
-	local BarType = self.BarType
-	local Current, Max
+local function IsMaxLevel()
+	if UnitLevel("player") == MAX_PLAYER_LEVEL then return true end
+end
 
-	if (self == Experience.XPBar1) then
-		GameTooltip:SetOwner(self, "ANCHOR_LEFT", 0, -5)
+local backdrop = CreateFrame("Frame", "Experience_Backdrop", UIParent)
+backdrop:SetSize(barWidth, barHeight)
+backdrop:SetPoint("TOPLEFT", Minimap, "BOTTOMLEFT", -1, -28)
+backdrop:SetPoint("TOPRIGHT", Minimap, "BOTTOMRIGHT", 1, -28)
+backdrop:SetFrameStrata("LOW")
+backdrop:CreatePixelShadow()
+
+local backdropBG = CreateFrame("Frame", "Experience_BackdropBG", backdrop)
+backdropBG:SetFrameLevel(backdrop:GetFrameLevel() - 1)
+backdropBG:SetPoint("TOPLEFT", -1, 1)
+backdropBG:SetPoint("BOTTOMRIGHT", 1, -1)
+backdropBG:SetBackdrop(K.BorderBackdrop)
+backdropBG:SetBackdropColor(unpack(C.Media.Backdrop_Color))
+
+local xpBar = CreateFrame("StatusBar", "Experience_xpBar", backdrop, "TextStatusBar")
+xpBar:SetWidth(barWidth)
+xpBar:SetHeight(GetWatchedFactionInfo() and (barHeight) or barHeight)
+xpBar:SetPoint("TOP", backdrop,"TOP", 0, 0)
+xpBar:SetStatusBarTexture(barTex)
+xpBar:SetStatusBarColor(31/255, 41/255, 130/255)
+--if C.Experience.ClassColor then xpBar:SetStatusBarColor(color.r, color.g, color.b) else xpBar:SetStatusBarColor(31/255, 41/255, 130/255) end
+
+local restedxpBar = CreateFrame("StatusBar", "Experience_restedxpBar", backdrop, "TextStatusBar")
+restedxpBar:SetHeight(GetWatchedFactionInfo() and (barHeight) or barHeight)
+restedxpBar:SetWidth(barWidth)
+restedxpBar:SetPoint("TOP", backdrop, "TOP", 0, 0)
+restedxpBar:SetStatusBarTexture(barTex)
+restedxpBar:Hide()
+
+local icon = xpBar:CreateTexture(nil, "OVERLAY")
+icon:SetSize(16, 16)
+icon:SetPoint("LEFT", xpBar, 3, 1)
+icon:SetTexture([=[Interface\CharacterFrame\UI-StateIcon]=])
+icon:SetTexCoord(0, .5, 0, .421875)
+
+local repBar = CreateFrame("StatusBar", "Experience_repBar", backdrop, "TextStatusBar")
+repBar:SetWidth(barWidth)
+repBar:SetHeight(IsMaxLevel() and barHeight - 0 or 0)
+repBar:SetPoint("BOTTOM", backdrop, "BOTTOM", 0, 0)
+repBar:SetStatusBarTexture(barTex)
+
+local mouseFrame = CreateFrame("Frame", "Experience_mouseFrame", backdrop)
+mouseFrame:SetAllPoints(backdrop)
+mouseFrame:EnableMouse(true)
+mouseFrame:SetFrameLevel(3)
+
+local function updateStatus()
+	local XP, maxXP, restXP = UnitXP("player"), UnitXPMax("player"), GetXPExhaustion()
+	if not maxXP or maxXP == 0 then return end
+	local percXP = math.floor((XP / maxXP) * 100)
+
+	if IsMaxLevel() then
+		xpBar:Hide()
+		restedxpBar:Hide()
+		repBar:SetHeight(barHeight)
+		if not GetWatchedFactionInfo() then backdrop:Hide() else backdrop:Show() end
 	else
-		GameTooltip:SetOwner(self, "ANCHOR_BOTTOMLEFT", 0, 5)
-	end
+		xpBar:SetMinMaxValues(min(0, XP), maxXP)
+		xpBar:SetValue(XP)
 
-	if BarType == "XP" then
-		local Rested = GetXPExhaustion()
-		local IsRested = GetRestState()
-
-		Current, Max = Experience:GetExperience()
-
-		if Max == 0 then
-			return
-		end
-
-		GameTooltip:AddLine(string.format("|cff0090FF"..XP..": %d / %d (%d%% - %d/%d)|r", Current, Max, Current / Max * 100, Bars - (Bars * (Max - Current) / Max), Bars))
-
-		if (IsRested == 1 and Rested) then
-			GameTooltip:AddLine(string.format("|cff4BAF4C"..TUTORIAL_TITLE26..": +%d (%d%%)|r", Rested, Rested / Max * 100))
-		end
-	elseif BarType == "ARTIFACT" then
-		Current, Max = Experience:GetArtifact()
-
-		if Max == 0 then
-			return
-		end
-
-		GameTooltip:AddLine(string.format("|cffe6cc80"..ARTIFACT_POWER..": %d / %d (%d%% - %d/%d)|r", Current, Max, Current / Max * 100, Bars - (Bars * (Max - Current) / Max), Bars))
-	else
-		local Level = UnitHonorLevel("player")
-		local LevelMax = GetMaxPlayerHonorLevel()
-		local Prestige = UnitPrestige("player")
-
-		Current, Max = Experience:GetHonor()
-
-		if Max == 0 then
-			GameTooltip:AddLine(PVP_HONOR_PRESTIGE_AVAILABLE)
-			GameTooltip:AddLine(PVP_HONOR_XP_BAR_CANNOT_PRESTIGE_HERE)
+		if restXP then
+			restedxpBar:Show()
+			local r, g, b = color.r, color.g, color.b
+			restedxpBar:SetStatusBarColor(r, g, b, .40)
+			restedxpBar:SetMinMaxValues(min(0, XP), maxXP)
+			restedxpBar:SetValue(XP + restXP)
+			icon:Show()
 		else
-			GameTooltip:AddLine(string.format("|cffee2222"..HONOR..": %d / %d (%d%% - %d/%d)|r", Current, Max, Current / Max * 100, Bars - (Bars * (Max - Current) / Max), Bars))
-			GameTooltip:AddLine(string.format("|cffcccccc"..RANK..": %d / %d|r", Level, LevelMax))
-			GameTooltip:AddLine(string.format("|cffcccccc"..PVP_PRESTIGE_RANK_UP_TITLE..": %d|r", Prestige))
-		end
-	end
-
-	GameTooltip:Show()
-end
-
-function Experience:GetExperience()
-	return UnitXP("player"), UnitXPMax("player")
-end
-
-function Experience:GetArtifact()
-	local itemID, altItemID, name, icon, totalXP, pointsSpent, quality, artifactAppearanceID, appearanceModID, itemAppearanceID, altItemAppearanceID, altOnTop = C_ArtifactUI.GetEquippedArtifactInfo()
-	local numPointsAvailableToSpend, xp, xpForNextPoint = MainMenuBar_GetNumArtifactTraitsPurchasableFromXP(pointsSpent, totalXP)
-
-	return xp, xpForNextPoint
-end
-
-function Experience:GetHonor()
-	return UnitHonor("player"), UnitHonorMax("player")
-end
-
-function Experience:Update(event, owner)
-	if (event == "UNIT_INVENTORY_CHANGED" and owner ~= "player") then
-		return
-	end
-
-	local ShowArtifact = HasArtifactEquipped()
-	local PlayerLevel = UnitLevel("player")
-
-	local Current, Max = self:GetExperience()
-	local Rested = GetXPExhaustion()
-	local IsRested = GetRestState()
-
-	for i = 1, self.NumBars do
-		local Bar = self["XPBar"..i]
-		local RestedBar = self["RestedBar"..i]
-		local r, g, b
-
-		Bar.BarType = "XP"
-
-		if (i == 1 and PlayerLevel == MAX_PLAYER_LEVEL) then
-			Current, Max = self:GetHonor()
-
-			Bar.BarType = "HONOR"
-		elseif (i == 2) then
-			if ShowArtifact then
-				Current, Max = self:GetArtifact()
-
-				Bar.BarType = "ARTIFACT"
-			else
-				Current, Max = self:GetHonor()
-
-				Bar.BarType = "HONOR"
-			end
+			restedxpBar:Hide()
+			icon:Hide()
 		end
 
-		local BarType = Bar.BarType
-
-		Bar:SetMinMaxValues(0, Max)
-		Bar:SetValue(Current)
-
-		if (BarType == "XP" and IsRested == 1 and Rested) then
-			RestedBar:Show()
-			RestedBar:SetMinMaxValues(0, Max)
-			RestedBar:SetValue(Rested + Current)
+		if GetWatchedFactionInfo() then
+			xpBar:SetHeight(barHeight)
+			restedxpBar:SetHeight(barHeight)
+			repBar:SetHeight(2)
+			repBar:Show()
 		else
-			RestedBar:Hide()
+			xpBar:SetHeight(barHeight)
+			restedxpBar:SetHeight(barHeight)
+			repBar:Hide()
 		end
-
-		if BarType == "XP" then
-			r, g, b = unpack(self.XPColor)
-		elseif BarType == "ARTIFACT" then
-			r, g, b = unpack(self.AFColor)
-		else
-			r, g, b = unpack(self.HNColor)
-		end
-
-		Bar:SetStatusBarColor(r, g, b)
 	end
+
+	if GetWatchedFactionInfo() then
+		local name, rank, minRep, maxRep, value = GetWatchedFactionInfo()
+		repBar:SetMinMaxValues(minRep, maxRep)
+		repBar:SetValue(value)
+		repBar:SetStatusBarColor(unpack(FactionInfo[rank][1]))
+	end
+
+	mouseFrame:SetScript("OnEnter", function()
+		GameTooltip:SetOwner(mouseFrame, "ANCHOR_TOPLEFT", -2, 5)
+		GameTooltip:ClearLines()
+		if not IsMaxLevel() then
+			GameTooltip:AddLine(L_EXPERIENCE_BAR)
+			GameTooltip:AddLine(string.format(L_EXPERIENCE_XP, K.Comma(XP), K.Comma(maxXP), (XP / maxXP) * 100))
+			GameTooltip:AddLine(string.format(L_EXPERIENCE_XPREMAINING, K.Comma(maxXP - XP)))
+			if restXP then GameTooltip:AddLine(string.format(L_EXPERIENCE_XPRESTED, K.Comma(restXP), restXP / maxXP * 100)) end
+		end
+		if GetWatchedFactionInfo() then
+			local name, rank, min, max, value = GetWatchedFactionInfo()
+			if not IsMaxLevel() then GameTooltip:AddLine(" ") end
+			GameTooltip:AddLine(string.format(L_REPUTATION_FCTITLE, name))
+			GameTooltip:AddLine(string.format(L_REPUTATION_STANDING..colorize(rank).. " %s|r", FactionInfo[rank][2]))
+			GameTooltip:AddLine(string.format(L_REPUTATION_REP, K.Comma(value - min), K.Comma(max - min), (value - min)/(max - min) * 100))
+			GameTooltip:AddLine(string.format(L_REPUTATION_REMAINGING, K.Comma(max - value)))
+		end
+		GameTooltip:Show()
+	end)
+	mouseFrame:SetScript("OnLeave", function() GameTooltip:Hide() end)
 end
 
-function Experience:Create()
-	for i = 1, self.NumBars do
-		local XPBar = CreateFrame("StatusBar", nil, UIParent)
-		local XPBarBG = CreateFrame("Frame", "StatusBarBG", XPBar)
-		local RestedBar = CreateFrame("StatusBar", nil, UIParent)
-
-		XPBar:SetStatusBarTexture(C.Media.Texture)
-		XPBar:EnableMouse()
-		XPBar:SetFrameStrata("BACKGROUND")
-		XPBar:CreatePixelShadow()
-		XPBar:SetFrameLevel(2)
-		XPBar:SetScript("OnEnter", Experience.SetTooltip)
-		XPBar:SetScript("OnLeave", HideTooltip)
-
-		RestedBar:SetStatusBarTexture(C.Media.Texture)
-		RestedBar:SetFrameStrata("BACKGROUND")
-		RestedBar:SetStatusBarColor(unpack(self.RestedColor))
-		RestedBar:SetAllPoints(XPBar)
-		RestedBar:SetFrameLevel(XPBar:GetFrameLevel())
-
-		XPBarBG:SetFrameLevel(XPBar:GetFrameLevel() - 1)
-		XPBarBG:SetPoint("TOPLEFT", -1, 1)
-		XPBarBG:SetPoint("BOTTOMRIGHT", 1, -1)
-		XPBarBG:SetBackdrop(K.BorderBackdrop)
-		XPBarBG:SetBackdropColor(unpack(C.Media.Backdrop_Color))
-
-		XPBar:SetSize(C.ExpRep.ExpWidth, C.ExpRep.ExpHeight)
-		XPBar:SetPoint("CENTER", ExperienceAnchor, "CENTER", 0, 0)
-
-		self["XPBar"..i] = XPBar
-		self["RestedBar"..i] = RestedBar
-	end
-
-	self:RegisterEvent("PLAYER_XP_UPDATE")
-	self:RegisterEvent("PLAYER_LEVEL_UP")
-	self:RegisterEvent("UPDATE_EXHAUSTION")
-	self:RegisterEvent("PLAYER_ENTERING_WORLD")
-	self:RegisterEvent("PLAYER_UPDATE_RESTING")
-	self:RegisterEvent("ARTIFACT_XP_UPDATE")
-	self:RegisterEvent("UNIT_INVENTORY_CHANGED")
-	self:RegisterEvent("HONOR_XP_UPDATE")
-	self:RegisterEvent("HONOR_LEVEL_UPDATE")
-	self:RegisterEvent("HONOR_PRESTIGE_UPDATE")
-
-	self:SetScript("OnEvent", self.Update)
-end
-
-function Experience:Enable()
-	if not self.IsCreated then
-		self:Create()
-
-		self.IsCreated = true
-	end
-
-	for i = 1, self.NumBars do
-		if not self["XPBar"..i]:IsShown() then
-			self["XPBar"..i]:Show()
-		end
-
-		if not self["RestedBar"..i]:IsShown() then
-			self["RestedBar"..i]:Show()
-		end
-	end
-end
-
-function Experience:Disable()
-	for i = 1, self.NumBars do
-		if self["XPBar"..i]:IsShown() then
-			self["XPBar"..i]:Hide()
-		end
-
-		if self["RestedBar"..i]:IsShown() then
-			self["RestedBar"..i]:Hide()
-		end
-	end
-end
-
-Experience:Enable()
+local frame = CreateFrame("Frame",nil,UIParent)
+frame:RegisterEvent("PLAYER_LEVEL_UP")
+frame:RegisterEvent("PLAYER_XP_UPDATE")
+frame:RegisterEvent("UPDATE_EXHAUSTION")
+frame:RegisterEvent("CHAT_MSG_COMBAT_FACTION_CHANGE")
+frame:RegisterEvent("UPDATE_FACTION")
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:SetScript("OnEvent", updateStatus)
