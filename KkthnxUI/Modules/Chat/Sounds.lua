de62a4530f6e5d3642103c53bd203de3ce99704c
Kkthnx <JRussell20@gmail.com>
2016-08-30 20:05:49 -0400

    Cleanup

--- KkthnxUI/Modules/Chat/Sounds.lua
+++ KkthnxUI/Modules/Chat/Sounds.lua
@@ -1,20 +1,16 @@
 ﻿local K, C, L, _ = select(2, ...):unpack()
 if C.Chat.Enable ~= true or C.Chat.WhispSound ~= true then return end
 
--- LUA API
-local sub = string.sub
-
 -- WOW API
 local CreateFrame = CreateFrame
 local PlaySoundFile = PlaySoundFile
 
 -- PLAY SOUND FILES SYSTEM(BY TUKZ)
-local SoundSys = CreateFrame("Frame")
-SoundSys:RegisterEvent("CHAT_MSG_WHISPER")
-SoundSys:RegisterEvent("CHAT_MSG_BN_WHISPER")
-SoundSys:HookScript("OnEvent", function(self, event, msg, ...)
-	if event == "CHAT_MSG_WHISPER" or event == "CHAT_MSG_BN_WHISPER" then
-		if (msg:sub(1, 3) == "OQ,") then return false, msg, ... end
+local Whisper = CreateFrame("Frame")
+Whisper:RegisterEvent("CHAT_MSG_WHISPER")
+Whisper:RegisterEvent("CHAT_MSG_BN_WHISPER")
+Whisper:HookScript("OnEvent", function(self, event)
+	if (event == "CHAT_MSG_WHISPER" or event == "CHAT_MSG_BN_WHISPER") then
 		PlaySoundFile(C.Media.Whisp_Sound, "Master")
 	end
 end)
