15f8e1d33c366d339398c8270f161c8c2ac18f36
Kkthnx <JRussell20@gmail.com>
2016-09-13 16:16:39 -0400

    Another attempt at fixing this blocked shit.

--- KkthnxUI/Modules/ActionBars/Bar1.lua
+++ KkthnxUI/Modules/ActionBars/Bar1.lua
@@ -14,19 +14,6 @@ local HasVehicleActionBar = HasVehicleActionBar
 local bar = CreateFrame("Frame", "Bar1Holder", ActionBarAnchor, "SecureHandlerStateTemplate")
 bar:SetAllPoints(ActionBarAnchor)
 
-for i = 1, 12 do
-	local button = _G["ActionButton"..i]
-	button:SetSize(C.ActionBar.ButtonSize, C.ActionBar.ButtonSize)
-	button:ClearAllPoints()
-	button:SetParent(Bar1Holder)
-	if i == 1 then
-		button:SetPoint("BOTTOMLEFT", Bar1Holder, 0, 0)
-	else
-		local previous = _G["ActionButton"..i-1]
-		button:SetPoint("LEFT", previous, "RIGHT", C.ActionBar.ButtonSpace, 0)
-	end
-end
-
 local Page = {
 	["DRUID"] = "[bonusbar:1,nostealth] 7; [bonusbar:1,stealth] 8; [bonusbar:2] 8; [bonusbar:3] 9; [bonusbar:4] 10;",
 	["ROGUE"] = "[bonusbar:1] 7;",
@@ -45,35 +32,50 @@ local function GetBar()
 end
 
 bar:RegisterEvent("PLAYER_LOGIN")
+bar:RegisterEvent("PLAYER_ENTERING_WORLD")
 bar:RegisterEvent("UPDATE_VEHICLE_ACTIONBAR")
---bar:RegisterEvent("UPDATE_OVERRIDE_ACTIONBAR")
+bar:RegisterEvent("UPDATE_OVERRIDE_ACTIONBAR")
 bar:RegisterEvent("CURRENCY_DISPLAY_UPDATE")
 bar:RegisterEvent("BAG_UPDATE")
 bar:SetScript("OnEvent", function(self, event, unit, ...)
-	if (event == "PLAYER_LOGIN" or event == "ACTIVE_TALENT_GROUP_CHANGED") then
+	if event == "PLAYER_LOGIN" or event == "ACTIVE_TALENT_GROUP_CHANGED" then
 		for i = 1, NUM_ACTIONBAR_BUTTONS do
 			local button = _G["ActionButton"..i]
 			self:SetFrameRef("ActionButton"..i, button)
 		end
 
 		self:Execute([[
-			buttons = table.new()
-			for i = 1, 12 do
-				table.insert(buttons, self:GetFrameRef("ActionButton"..i))
-			end
+		buttons = table.new()
+		for i = 1, 12 do
+			table.insert(buttons, self:GetFrameRef("ActionButton"..i))
+		end
 		]])
 
 		self:SetAttribute("_onstate-page", [[
-			for i, button in ipairs(buttons) do
-				button:SetAttribute("actionpage", tonumber(newstate))
-			end
+		for i, button in ipairs(buttons) do
+			button:SetAttribute("actionpage", tonumber(newstate))
+		end
 		]])
 
 		RegisterStateDriver(self, "page", GetBar())
-	--elseif event == "UPDATE_VEHICLE_ACTIONBAR" or event == "UPDATE_OVERRIDE_ACTIONBAR" then
-	elseif event == "UPDATE_VEHICLE_ACTIONBAR" then
-		--if not InCombatLockdown() and (HasVehicleActionBar() or HasOverrideActionBar()) then
-		if not InCombatLockdown() and (HasVehicleActionBar()) then
+	elseif event == "PLAYER_ENTERING_WORLD" then
+		local button
+		for i = 1, 12 do
+			local button = _G["ActionButton"..i]
+			button:SetSize(C.ActionBar.ButtonSize, C.ActionBar.ButtonSize)
+			button:ClearAllPoints()
+			button:SetParent(Bar1Holder)
+			button:SetFrameStrata("BACKGROUND")
+			button:SetFrameLevel(15)
+			if i == 1 then
+				button:SetPoint("BOTTOMLEFT", Bar1Holder, 0, 0)
+			else
+				local previous = _G["ActionButton"..i-1]
+				button:SetPoint("LEFT", previous, "RIGHT", C.ActionBar.ButtonSpace, 0)
+			end
+		end
+	elseif event == "UPDATE_VEHICLE_ACTIONBAR" or event == "UPDATE_OVERRIDE_ACTIONBAR" then
+		if not InCombatLockdown() and (HasVehicleActionBar() or HasOverrideActionBar()) then
 			for i = 1, NUM_ACTIONBAR_BUTTONS do
 				local button = _G["ActionButton"..i]
 				ActionButton_Update(button)
