2287635ec1baca4413d6091ea4ccd6f02965bd88
Kkthnx <JRussell20@gmail.com>
2016-09-14 22:10:25 -0400

    Credits.

--- KkthnxUI/Modules/UnitFrames/Plugins/ClassIcons.lua
+++ KkthnxUI/Modules/UnitFrames/Plugins/ClassIcons.lua
@@ -2,6 +2,7 @@ local K, C, L, _ = select(2, ...):unpack()
 if C.Unitframe.ClassIcon ~= true then return end
 
 -- CLASS ICONS
+-- CREDITS TO GOSSIPGIRLXO FOR THE DARK FLATICONS.
 hooksecurefunc("UnitFramePortrait_Update", function(self)
 	if self.portrait then
 		if UnitIsPlayer(self.unit) then
