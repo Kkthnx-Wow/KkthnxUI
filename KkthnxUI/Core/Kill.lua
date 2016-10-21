4c324f6d1526dd17ce70e7f32a851f5101d79b5f
Kkthnx <JRussell20@gmail.com>
2016-10-21 17:28:33 -0400

    6.04 with some fixes

--- KkthnxUI/Core/Kill.lua
+++ KkthnxUI/Core/Kill.lua
@@ -14,63 +14,30 @@ DisableBlizzard:SetScript("OnEvent", function(self, event)
 			hooksecurefunc("AchievementFrameCategories_DisplayButton", function(button) button.showTooltipFunc = nil end)
 		end
 	end
-
+	
 	if C.Unitframe.Enable then
 		function PetFrame_Update() end
 		function PlayerFrame_AnimateOut() end
 		function PlayerFrame_AnimFinished() end
 		function PlayerFrame_ToPlayerArt() end
 		function PlayerFrame_ToVehicleArt() end
-
-		for i = 1, MAX_BOSS_FRAMES do
-			local Boss = _G["Boss"..i.."TargetFrame"]
-			local Health = _G["Boss"..i.."TargetFrame".."HealthBar"]
-			local Power = _G["Boss"..i.."TargetFrame".."ManaBar"]
-
-			Boss:UnregisterAllEvents()
-			Boss.Show = K.Noop
-			Boss:Hide()
-
-			Health:UnregisterAllEvents()
-			Power:UnregisterAllEvents()
-		end
-
-		for i = 1, MAX_PARTY_MEMBERS do
-			local PartyMember = _G["PartyMemberFrame" .. i]
-			local Health = _G["PartyMemberFrame" .. i .. "HealthBar"]
-			local Power = _G["PartyMemberFrame" .. i .. "ManaBar"]
-			local Pet = _G["PartyMemberFrame" .. i .."PetFrame"]
-			local PetHealth = _G["PartyMemberFrame" .. i .."PetFrame" .. "HealthBar"]
-
-			PartyMember:UnregisterAllEvents()
-			PartyMember:SetParent(UIFrameHider)
-			PartyMember:Hide()
-			Health:UnregisterAllEvents()
-			Power:UnregisterAllEvents()
-
-			Pet:UnregisterAllEvents()
-			Pet:SetParent(UIFrameHider)
-			PetHealth:UnregisterAllEvents()
-
-			HidePartyFrame()
-			ShowPartyFrame = K.Noop
-			HidePartyFrame = K.Noop
-		end
-
-		if C.Raidframe.Enable then
-			InterfaceOptionsFrameCategoriesButton10:SetHeight(0.00001)
-			InterfaceOptionsFrameCategoriesButton10:SetAlpha(0)
-
-			if CompactRaidFrameManager then
-				CompactRaidFrameManager:SetParent(UIFrameHider)
-			end
-
-			if CompactUnitFrameProfiles then
-				CompactUnitFrameProfiles:UnregisterAllEvents()
-			end
-		end
+		
+		ShowPartyFrame = K.Noop
+		HidePartyFrame = K.Noop
+		
+		CompactUnitFrameProfiles_ApplyProfile = K.Noop
 	end
-
+	
+	if C.Raidframe.Enable then
+		if not InCombatLockdown() then
+			CompactRaidFrameManager:Kill()
+			CompactRaidFrameContainer:Kill()
+		end
+		
+		CompactRaidFrameManager_UpdateShown = K.Noop
+		CompactRaidFrameManager_UpdateOptionsFlowContainer = K.Noop
+	end
+	
 	if C.Minimap.Garrison == true then
 		GarrisonLandingPageTutorialBox:Kill()
 	end
@@ -79,29 +46,29 @@ DisableBlizzard:SetScript("OnEvent", function(self, event)
 	HelpPlateTooltip:Kill()
 	TalentMicroButtonAlert:Kill()
 	EJMicroButtonAlert:Kill()
-
+	
 	if C.Unitframe.Enable then
 		InterfaceOptionsCombatPanelTargetOfTarget:Kill()
 		SetCVar("showPartyBackground", 0)
 	end
-
+	
 	if C.Cooldown.Enable then
 		SetCVar("countdownForCooldowns", 0)
 		InterfaceOptionsActionBarsPanelCountdownCooldowns:Kill()
 	end
-
+	
 	if C.Nameplates.Enable then
 		SetCVar("ShowClassColorInNameplate", 1)
 	end
-
+	
 	if C.Chat.Enable then
 		SetCVar("chatStyle", "im")
 	end
-
+	
 	if C.Minimap.Enable then
 		InterfaceOptionsDisplayPanelRotateMinimap:Kill()
 	end
-
+	
 	if C.ActionBar.Enable then
 		InterfaceOptionsActionBarsPanelBottomLeft:Kill()
 		InterfaceOptionsActionBarsPanelBottomRight:Kill()
