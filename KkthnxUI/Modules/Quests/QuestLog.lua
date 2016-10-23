607ce2c723484c17d7c4a20726e13e3457d0f8c7
Kkthnx <JRussell20@gmail.com>
2016-10-22 23:33:40 -0400

    More for 7.1.0, Still works with Live 7.0.3

--- KkthnxUI/Modules/Quests/QuestLog.lua
+++ KkthnxUI/Modules/Quests/QuestLog.lua
@@ -1,25 +1,26 @@
 local K, C, L = select(2, ...):unpack()
 
 local pairs = pairs
-local strmatch = string.match
 
 local hooksecurefunc = hooksecurefunc
 local IsAltKeyDown, IsControlKeyDown = IsAltKeyDown, IsControlKeyDown
 
 -- QUEST LEVEL(YQUESTLEVEL BY YLEAF)
-hooksecurefunc("QuestLogQuests_Update", function()
-	for i, button in pairs(QuestMapFrame.QuestsFrame.Contents.Titles) do
-		if button:IsShown() then
-			local level = strmatch(GetQuestLink(button.questLogIndex), "quest:%d+:(%d+)")
-			if level then
-				local height = button.Text:GetHeight()
-				button.Text:SetFormattedText("[%d] %s", level, button.Text:GetText())
-				button.Check:SetPoint("LEFT", button.Text, button.Text:GetWrappedWidth() + 2, 0)
-				button:SetHeight(button:GetHeight() - height + button.Text:GetHeight())
+if K.WoWPatch == ("7.0.3") then
+	hooksecurefunc("QuestLogQuests_Update", function()
+		for i, button in pairs(QuestMapFrame.QuestsFrame.Contents.Titles) do
+			if button:IsShown() then
+				local level = strmatch(GetQuestLink(button.questLogIndex), "quest:%d+:(%d+)")
+				if level then
+					local height = button.Text:GetHeight()
+					button.Text:SetFormattedText("[%d] %s", level, button.Text:GetText())
+					button.Check:SetPoint("LEFT", button.Text, button.Text:GetWrappedWidth() + 2, 0)
+					button:SetHeight(button:GetHeight() - height + button.Text:GetHeight())
+				end
 			end
 		end
-	end
-end)
+	end)
+end
 
 -- CTRL+CLICK TO ABANDON A QUEST OR ALT+CLICK TO SHARE A QUEST(BY SUICIDAL KATT)
 hooksecurefunc("QuestMapLogTitleButton_OnClick", function(self)
