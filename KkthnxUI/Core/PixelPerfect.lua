5f4cdf63be8211802c5aecc5b3b6cccc6531fa80
Kkthnx <JRussell20@gmail.com>
2016-12-15 21:25:17 -0500

    Updates.

--- KkthnxUI/Core/PixelPerfect.lua
+++ KkthnxUI/Core/PixelPerfect.lua
@@ -13,11 +13,8 @@ local StaticPopup_Show = StaticPopup_Show
 -- Global variables that we don't cache, list them here for mikk's FindGlobals script
 -- GLOBALS: ForceQuit, WorldMapFrame, UIParent
 
+local Lock = false
 local RequireRestart = false
-
-if C.General.AutoScale then
-	C.General.UIScale = min(2, max(0.32, 768 / string.match(K.Resolution, "%d+x(%d+)")))
-end
 
 StaticPopupDialogs["CLIENT_RESTART"] = {
 	text = L.Popup.ResolutionChanged,
@@ -34,78 +31,56 @@ StaticPopupDialogs["CLIENT_RESTART"] = {
 -- Optimize graphic after we enter world
 local PixelPerfect = CreateFrame("Frame")
 PixelPerfect:RegisterEvent("PLAYER_ENTERING_WORLD")
+PixelPerfect:RegisterEvent("CINEMATIC_STOP")
+PixelPerfect:RegisterEvent("UI_SCALE_CHANGED")
+PixelPerfect:RegisterEvent("DISPLAY_SIZE_CHANGED")
 PixelPerfect:SetScript("OnEvent", function(self, event)
-	if (event == "DISPLAY_SIZE_CHANGED") then
-		if C.General.AutoScale and not RequireRestart then
-			StaticPopup_Show("CLIENT_RESTART")
-		end
-
-		RequireRestart = true
-	else
-		-- Make sure that UI scaling is turned on
-		local UseUIScale = GetCVarBool("useUiScale")
-		if not UseUIScale then
-			SetCVar("useUiScale", 1)
-		end
-
-		if (format("%.2f", GetCVar("uiScale")) ~= format("%.2f", C.General.UIScale)) then
-			SetCVar("uiScale", C.General.UIScale)
-		end
-
-		-- Allow 4K and WQHD resolution to have an uiScale lower than 0.64, which is
-		-- The lowest value of UIParent scale by default
-		if (C.General.UIScale < 0.64) then
-			UIParent:SetScale(C.General.UIScale)
-		end
-
-		self:UnregisterEvent("PLAYER_ENTERING_WORLD")
-		self:RegisterEvent("DISPLAY_SIZE_CHANGED")
+	-- Prevent a C stack overflow
+	if Lock then
+		return
 	end
-end)
+	Lock = true
 
--- Optimize graphic after we enter world
--- local PixelPerfect = CreateFrame("Frame")
--- PixelPerfect:RegisterEvent("PLAYER_ENTERING_WORLD")
--- PixelPerfect:RegisterEvent("CINEMATIC_STOP")
--- PixelPerfect:RegisterEvent("UI_SCALE_CHANGED")
--- PixelPerfect:RegisterEvent("DISPLAY_SIZE_CHANGED")
--- PixelPerfect:SetScript("OnEvent", function(self, event)
--- 	if InCombatLockdown() then
--- 		self:RegisterEvent("PLAYER_REGEN_ENABLED")
--- 		return
--- 	end
--- 	if event ~= "CINEMATIC_STOP" and InCinematic() then return end
---
--- 	-- Make sure that UI scaling is turned on
--- 	local UseUIScale = GetCVarBool("useUiScale")
--- 	if not UseUIScale then
--- 		SetCVar("useUiScale", 1)
--- 	end
---
--- 	-- Automatically change the scale if auto scaling is activated
--- 	if C.General.AutoScale then
--- 		if (format("%.2f", GetCVar("uiScale")) ~= format("%.2f", C.General.UIScale)) then
--- 			SetCVar("uiScale", C.General.UIScale)
--- 			if not RequireRestart then
--- 				StaticPopup_Show("CLIENT_RESTART")
--- 				RequireRestart = true
--- 			end
--- 		end
---
--- 		-- Allow 4K and WQHD resolution to have an uiScale lower than 0.64, which is
--- 		-- The lowest value of UIParent scale by default
--- 		if (C.General.UIScale < 0.64) then
--- 			UIParent:SetScale(C.General.UIScale)
--- 			if not RequireRestart then
--- 				StaticPopup_Show("CLIENT_RESTART")
--- 				RequireRestart = true
--- 			end
--- 		end
--- 	end
---
--- 	if event == "PLAYER_ENTERING_WORLD" then
--- 		self:UnregisterEvent("PLAYER_ENTERING_WORLD")
--- 	elseif event == "PLAYER_REGEN_ENABLED" then
--- 		self:UnregisterEvent("PLAYER_REGEN_ENABLED")
--- 	end
--- end)
+	if InCombatLockdown() then
+		self:RegisterEvent("PLAYER_REGEN_ENABLED")
+		return
+	end
+	if event ~= "CINEMATIC_STOP" and InCinematic() then return end
+
+	-- Make sure that UI scaling is turned on
+	local UseUIScale = GetCVarBool("useUiScale")
+	if not UseUIScale then
+		SetCVar("useUiScale", 1)
+	end
+
+	-- Automatically change the scale if auto scaling is activated
+	if C.General.AutoScale then
+		C.General.UIScale = min(2, max(0.32, 768 / string.match(K.Resolution, "%d+x(%d+)")))
+	end
+
+	if (format("%.2f", GetCVar("uiScale")) ~= format("%.2f", C.General.UIScale)) then
+		SetCVar("uiScale", C.General.UIScale)
+		if not RequireRestart then
+			StaticPopup_Show("CLIENT_RESTART")
+			RequireRestart = true
+		end
+	end
+
+	-- Allow 4K and WQHD resolution to have an uiScale lower than 0.64, which is
+	-- The lowest value of UIParent scale by default
+	if (C.General.UIScale < 0.64) then
+		UIParent:SetScale(C.General.UIScale)
+		if not RequireRestart then
+			StaticPopup_Show("CLIENT_RESTART")
+			RequireRestart = true
+		end
+	end
+
+	if event == "PLAYER_ENTERING_WORLD" then
+		self:UnregisterEvent("PLAYER_ENTERING_WORLD")
+	elseif event == "PLAYER_REGEN_ENABLED" then
+		self:UnregisterEvent("PLAYER_REGEN_ENABLED")
+	end
+
+	Lock = false
+end)
