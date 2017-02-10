1f27fc09e11df0a352aee1b1102eabaa2b68801f
Kkthnx <JRussell20@gmail.com>
2017-02-10 16:18:51 -0500

    Clean this file some.

--- KkthnxUI/Modules/Blizzard/ReputationGain.lua
+++ KkthnxUI/Modules/Blizzard/ReputationGain.lua
@@ -1,27 +1,31 @@
 local K, C, L = unpack(select(2, ...))
 if C.Blizzard.ReputationGain ~= true then return end
 
-local frame = 1
-local f = CreateFrame("FRAME", nil, UIParent)
-
-local factionVars = {}
-
-local cwhite, cyellow, corange, cresume = "|CFF999999", "|CFFD9C45C", "|CFFD9865C", "|r"
-local fmt = string.format
-local getfactioninfo = GetFactionInfo
+-- Lua API
+local _G = _G
 local math_abs = math.abs
 local math_ceil = math.ceil
+local string_format = string.format
+
+-- Wow API
+local GetFactionInfo = _G.GetFactionInfo
+local GetNumFactions = _G.GetNumFactions
+
+local factionVars = {}
+local corange = "|CFFD9865C"
+local cwhite = "|CFF999999"
+local cyellow = "|CFFD9C45C"
+local cresume = "|r"
+
+local factions = 0
+local init = 0
 local standingmax = 8
 local standingmin = 1
-local init = 0
-local factions = 0
-local chatframe = _G["ChatFrame" .. frame]
-f:RegisterEvent("UPDATE_FACTION")
 
 local function ScanFaction()
-	if (GetNumFactions() == 0) then DEFAULT_CHAT_FRAME:AddMessage("Factions not yet available") end
+	if (GetNumFactions() == 0) then print("Factions not yet available") end
 	for i = 1, GetNumFactions() do
-		local name, _, standingID, _, _, barValue, _, _, isHeader, _, hasRep = getfactioninfo(i)
+		local name, _, standingID, _, _, barValue, _, _, isHeader, _, hasRep = GetFactionInfo(i)
 		if ((not isHeader or hasRep) and name) then
 			factionVars[name] = {}
 			factionVars[name].Standing = standingID
@@ -43,18 +47,16 @@ local function Report()
 		factions = tempfactions
 	end
 	for factionIndex = 1, GetNumFactions() do
-		local name, _, standingID, barMin, barMax, barValue, _, _, isHeader, _, hasRep = getfactioninfo(factionIndex)
+		local name, _, standingID, barMin, barMax, barValue, _, _, isHeader, _, hasRep = GetFactionInfo(factionIndex)
 
 		if (not isHeader or hasRep) and factionVars[name] then
 			local diff = barValue - factionVars[name].Value
 			if diff ~= 0 then
 				if standingID ~= factionVars[name].Standing then
 					local newfaction = _G["FACTION_STANDING_LABEL" .. standingID]
-					local newstandingtext =
-					"New standing with " .. cyellow .. name .. cresume ..
-					" is " .. cyellow .. newfaction .. cresume .. "!"
+					local newstandingtext = "New standing with " .. cyellow .. name .. cresume .." is " .. cyellow .. newfaction .. cresume .. "!"
 
-					chatframe:AddMessage(newstandingtext)
+					print(newstandingtext)
 				end
 
 				local remaining, nextstanding, plusminus
@@ -77,11 +79,11 @@ local function Report()
 				local change = math_abs(barValue - factionVars[name].Value)
 				local repetitions = math_ceil(remaining / change)
 
-				local newvaluetext = fmt("%s%+d%s %s%s, %s%d%s more to %s%s%s (%s%d%s Repetitions).",
-				corange,change,cyellow,name,cresume,corange,remaining,cresume,
-				cyellow,nextstanding,cresume,corange,repetitions,cresume )
+				local newvaluetext = string_format("%s%+d%s %s%s, %s%d%s more to %s%s%s (%s%d%s Repetitions).",
+				corange, change, cyellow, name, cresume, corange, remaining, cresume,
+				cyellow, nextstanding, cresume, corange, repetitions, cresume)
 
-				chatframe:AddMessage(newvaluetext)
+				print(newvaluetext)
 
 				factionVars[name].Value = barValue
 				factionVars[name].Standing = standingID
@@ -90,8 +92,10 @@ local function Report()
 	end
 end
 
-local function eventHandler(self, event, ...)
-	if(event == "UPDATE_FACTION") then Report() end
-end
-
-f:SetScript("OnEvent", eventHandler)
+local Loading = CreateFrame("Frame")
+Loading:RegisterEvent("UPDATE_FACTION")
+Loading:SetScript("OnEvent", function(self, event)
+	if (event == "UPDATE_FACTION") then
+		Report()
+	end
+end)
