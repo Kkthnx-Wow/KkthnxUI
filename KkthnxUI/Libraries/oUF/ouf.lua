8fdb6d81dcfbb9a14aa75970176e8c3ee70cdc0c
Kkthnx <JRussell20@gmail.com>
2017-06-14 14:07:23 -0400

    Update to oUF Nameplates.

--- KkthnxUI/Libraries/oUF/ouf.lua
+++ KkthnxUI/Libraries/oUF/ouf.lua
@@ -614,6 +614,8 @@ function oUF:SpawnNamePlates(namePrefix, nameplateCallback, nameplateCVars)
 				Private.UpdateUnits(nameplate.unitFrame, unit)
 
 				walkObject(nameplate.unitFrame, unit)
+			else
+				Private.UpdateUnits(nameplate.unitFrame, unit)
 			end
 
 			nameplate.unitFrame:SetAttribute('unit', unit)
