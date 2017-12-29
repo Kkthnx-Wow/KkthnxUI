b1e27639ae4a53a531441c11c3390e66132e41fa
Kkthnx <Kkthnx@users.noreply.github.com>
2017-12-29 17:45:31 -0500

    Update Auras.lua

--- KkthnxUI/Modules/Auras/Auras.lua
+++ KkthnxUI/Modules/Auras/Auras.lua
@@ -1,58 +1,61 @@
 local K, C, L = unpack(select(2, ...))
-local A = K:NewModule("Auras", "AceEvent-3.0", "AceHook-3.0")
+local Module = K:NewModule("Auras", "AceEvent-3.0", "AceHook-3.0")
 
--- Lua API
 local _G = _G
-local next = next
-local select = select
-local unpack = unpack
+local GetTime = GetTime
+local select, unpack = select, unpack
+local floor = math.floor
 
--- Wow API
-local GetInventoryItemTexture = _G.GetInventoryItemTexture
-local GetTime = _G.GetTime
-local GetWeaponEnchantInfo = _G.GetWeaponEnchantInfo
-local UnitAura = _G.UnitAura
 local CreateFrame = _G.CreateFrame
-local UIParent = _G.UIParent
+local GetInventoryItemQuality = _G.GetInventoryItemQuality
+local GetInventoryItemTexture = _G.GetInventoryItemTexture
+local GetItemQualityColor = _G.GetItemQualityColor
+local GetWeaponEnchantInfo = _G.GetWeaponEnchantInfo
 local RegisterAttributeDriver = _G.RegisterAttributeDriver
+local RegisterStateDriver = _G.RegisterStateDriver
+local UnitAura = _G.UnitAura
 
--- Global variables that we don't cache, list them here for mikk's FindGlobals script
--- GLOBALS: BuffFrame, TemporaryEnchantFrame, InterfaceOptionsFrameCategoriesButton12
--- GLOBALS: DebuffTypeColor, PetBattleFrameHider, SecureHandlerSetFrameRef
+local DIRECTION_TO_POINT = {
+    DOWN_RIGHT = "TOPLEFT",
+    DOWN_LEFT = "TOPRIGHT",
+    UP_RIGHT = "BOTTOMLEFT",
+    UP_LEFT = "BOTTOMRIGHT",
+    RIGHT_DOWN = "TOPLEFT",
+    RIGHT_UP = "BOTTOMLEFT",
+    LEFT_DOWN = "TOPRIGHT",
+    LEFT_UP = "BOTTOMRIGHT",
+}
 
-local buttonsize = C["Auras"].ButtonSize -- Buff Size
-local spacing = C["Auras"].ButtonSpace -- Buff Spacing
-local buffsperrow = C["Auras"].ButtonPerRow
-local buffholder, debuffholder, enchantholder
-local BUFF_FLASH_TIME_ON = 0.75
-local BUFF_FLASH_TIME_OFF = 0.75
-local BUFF_MIN_ALPHA = 0.3
+local DIRECTION_TO_HORIZONTAL_SPACING_MULTIPLIER = {
+    DOWN_RIGHT = 1,
+    DOWN_LEFT = -1,
+    UP_RIGHT = 1,
+    UP_LEFT = -1,
+    RIGHT_DOWN = 1,
+    RIGHT_UP = 1,
+    LEFT_DOWN = -1,
+    LEFT_UP = -1,
+}
 
-function A:UpdateAlpha(elapsed)
-    self.BuffFrameFlashTime = self.BuffFrameFlashTime - elapsed
-    if (self.BuffFrameFlashTime < 0) then
-        local overtime = -self.BuffFrameFlashTime
-        if (self.BuffFrameFlashState == 0) then
-            self.BuffFrameFlashState = 1
-            self.BuffFrameFlashTime = BUFF_FLASH_TIME_ON
-        else
-            self.BuffFrameFlashState = 0
-            self.BuffFrameFlashTime = BUFF_FLASH_TIME_OFF
-        end
-        if (overtime < self.BuffFrameFlashTime) then
-            self.BuffFrameFlashTime = self.BuffFrameFlashTime - overtime
-        end
-    end
+local DIRECTION_TO_VERTICAL_SPACING_MULTIPLIER = {
+    DOWN_RIGHT = -1,
+    DOWN_LEFT = -1,
+    UP_RIGHT = 1,
+    UP_LEFT = 1,
+    RIGHT_DOWN = -1,
+    RIGHT_UP = 1,
+    LEFT_DOWN = -1,
+    LEFT_UP = 1,
+}
 
-    if ( self.BuffFrameFlashState == 1 ) then
-        self.BuffAlphaValue = (BUFF_FLASH_TIME_ON - self.BuffFrameFlashTime) / BUFF_FLASH_TIME_ON
-    else
-        self.BuffAlphaValue = self.BuffFrameFlashTime / BUFF_FLASH_TIME_ON
-    end
-    self.BuffAlphaValue = (self.BuffAlphaValue * (1 - BUFF_MIN_ALPHA)) + BUFF_MIN_ALPHA
-end
+local IS_HORIZONTAL_GROWTH = {
+    RIGHT_DOWN = true,
+    RIGHT_UP = true,
+    LEFT_DOWN = true,
+    LEFT_UP = true,
+}
 
-function A:UpdateTime(elapsed)
+function Module:UpdateTime(elapsed)
     if (self.offset) then
         local expiration = select(self.offset, GetWeaponEnchantInfo())
         if (expiration) then
@@ -60,80 +63,93 @@ function A:UpdateTime(elapsed)
         else
             self.timeLeft = 0
         end
+    else
+        self.timeLeft = self.timeLeft - elapsed
     end
 
-    if (self.timeLeft) then
-        self.timeLeft = math.max(self.timeLeft - elapsed, 0)
-        if (self.timeLeft <= 0) then
-            self:SetAlpha(1)
-            self.time:SetText("")
-        else
-            local time = K.FormatTime(self.timeLeft)
-            if self.timeLeft <= 86400.5 and self.timeLeft > 3600.5 then
-                self.time:SetText(NORMAL_FONT_COLOR_CODE..time.."|r")
-                self:SetAlpha(1)
-            elseif self.timeLeft <= 3600.5 and self.timeLeft > 60.5 then
-                self.time:SetText(NORMAL_FONT_COLOR_CODE..time.."|r")
-                self:SetAlpha(1)
-            elseif self.timeLeft <= 60.5 then
-                self.time:SetText("|cffff0000"..time.."|r")
-                if A.AlphaFrame then
-                    self:SetAlpha(A.AlphaFrame.BuffAlphaValue)
-                end
-            end
-        end
+    if (self.nextUpdate > 0) then
+        self.nextUpdate = self.nextUpdate - elapsed
+        return
+    end
+
+    local timerValue, formatID
+    timerValue, formatID, self.nextUpdate = K.GetTimeInfo(self.timeLeft, C["Auras"].FadeThreshold)
+    self.time:SetFormattedText(("%s%s|r"):format(K.TimeColors[formatID], K.TimeFormats[formatID][1]), timerValue)
+
+    if self.timeLeft > C["Auras"].FadeThreshold then
+        K.UIFrameStopFlash(self)
+    else
+        K.UIFrameFlash(self, 1)
     end
 end
 
-function A:CreateIcon(button)
+function Module:CreateIcon(button)
     button.texture = button:CreateTexture(nil, "BORDER")
-    button.texture:SetTexCoord(.08, .92, .08, .92)
     button.texture:SetAllPoints()
+    button.texture:SetTexCoord(K.TexCoords[1], K.TexCoords[2], K.TexCoords[3], K.TexCoords[4])
 
     button.count = button:CreateFontString(nil, "ARTWORK")
-    button.count:SetPoint("TOPRIGHT", 1, 1)
-    button.count:SetFont(C["Media"].Font, 12, "OUTLINE")
+    button.count:SetPoint("BOTTOMRIGHT", -1, 1)
+    button.count:FontTemplate(nil, 12, "OUTLINE")
 
     button.time = button:CreateFontString(nil, "ARTWORK")
-    button.time:SetPoint("CENTER", button, "BOTTOM", 0, -8)
-    button.time:SetFont(C["Media"].Font, 12, "OUTLINE")
+    button.time:SetPoint("TOP", button, "BOTTOM", 1, -4)
+    button.time:FontTemplate(nil, 12, "OUTLINE")
 
-    button:SetScript("OnUpdate", A.UpdateTime)
+    K.SetAnimationGroup(button)
 
-    button:SetTemplate("ActionButton", true)
+    button:SetScript("OnAttributeChanged", Module.OnAttributeChanged)
 
-    -- Really do not need this with our new SetTemplate
-    -- button.highlight = button:CreateTexture(nil, "HIGHLIGHT")
-    -- button.highlight:SetAllPoints(button.texture)
-    -- button.highlight:SetVertexColor(1, 1, 1)
-        -- button.highlight:SetTexture("Interface\\Buttons\\ButtonHilight-Square")
-        -- button.highlight:SetBlendMode("ADD")
+    local ButtonData = {
+        FloatingBG = nil,
+        Icon = button.texture,
+        Cooldown = nil,
+        Flash = nil,
+        Pushed = nil,
+        Normal = nil,
+        Disabled = nil,
+        Checked = nil,
+        Border = nil,
+        AutoCastable = nil,
+        Highlight = nil,
+        HotKey = nil,
+        Count = false,
+        Name = nil,
+        Duration = false,
+        AutoCast = nil,
+    }
 
-    button:SetScript("OnAttributeChanged", A.OnAttributeChanged)
+    local header = button:GetParent()
+    local auraType = header:GetAttribute("filter")
+
+    if auraType == "HELPFUL" then
+        button:SetTemplate("ActionButton", true)
+    elseif auraType == "HARMFUL" then
+        button:SetTemplate("ActionButton", true)
+    end
 end
 
-function A:UpdateAura(button, index)
+function Module:UpdateAura(button, index)
     local filter = button:GetParent():GetAttribute("filter")
     local unit = button:GetParent():GetAttribute("unit")
-    local name, rank, texture, count, dtype, duration, expirationTime, caster, isStealable, shouldConsolidate, spellID, canApplyAura, isBossDebuff = UnitAura(unit, index, filter)
+    local name, _, texture, count, dtype, duration, expirationTime = UnitAura(unit, index, filter)
 
     if (name) then
         if (duration > 0 and expirationTime) then
             local timeLeft = expirationTime - GetTime()
             if (not button.timeLeft) then
                 button.timeLeft = timeLeft
-                button:SetScript("OnUpdate", A.UpdateTime)
+                button:SetScript("OnUpdate", Module.UpdateTime)
             else
                 button.timeLeft = timeLeft
             end
 
             button.nextUpdate = -1
-            A.UpdateTime(button, 0)
+            Module.UpdateTime(button, 0)
         else
             button.timeLeft = nil
             button.time:SetText("")
             button:SetScript("OnUpdate", nil)
-            button:SetAlpha(1)
         end
 
         if (count > 1) then
@@ -143,9 +159,10 @@ function A:UpdateAura(button, index)
         end
 
         if filter == "HARMFUL" then
-            local color = DebuffTypeColor[dtype] or DebuffTypeColor.none
+            local color = DebuffTypeColor[dtype or ""]
             button:SetBackdropBorderColor(color.r, color.g, color.b)
-            button.texture:SetInside(button, 1, 1)
+        else
+            button:SetBackdropBorderColor(C["Media"].BorderColor[1], C["Media"].BorderColor[2], C["Media"].BorderColor[3])
         end
 
         button.texture:SetTexture(texture)
@@ -153,7 +170,7 @@ function A:UpdateAura(button, index)
     end
 end
 
-function A:UpdateTempEnchant(button, index)
+function Module:UpdateTempEnchant(button, index)
     local quality = GetInventoryItemQuality("player", index)
     button.texture:SetTexture(GetInventoryItemTexture("player", index))
 
@@ -161,7 +178,7 @@ function A:UpdateTempEnchant(button, index)
     local offset = 2
     local weapon = button:GetName():sub(-1)
     if weapon:match("2") then
-        offset = 5
+        offset = 6
     end
 
     if (quality) then
@@ -171,126 +188,125 @@ function A:UpdateTempEnchant(button, index)
     local expirationTime = select(offset, GetWeaponEnchantInfo())
     if (expirationTime) then
         button.offset = offset
-        button:SetScript("OnUpdate", A.UpdateTime)
+        button:SetScript("OnUpdate", Module.UpdateTime)
         button.nextUpdate = -1
-        A.UpdateTime(button, 0)
+        Module.UpdateTime(button, 0)
     else
         button.timeLeft = nil
         button.offset = nil
         button:SetScript("OnUpdate", nil)
-        button:SetAlpha(1)
         button.time:SetText("")
     end
 end
 
-function A:OnAttributeChanged(attribute, value)
+function Module:OnAttributeChanged(attribute, value)
     if (attribute == "index") then
-        A:UpdateAura(self, value)
+        Module:UpdateAura(self, value)
     elseif (attribute == "target-slot") then
-        A:UpdateTempEnchant(self, value)
+        Module:UpdateTempEnchant(self, value)
     end
 end
 
-function A:UpdateHeader(header)
-    header:SetAttribute("consolidateTo", 0)
-    header:SetAttribute("maxWraps", 3)
-    header:SetAttribute("sortMethod", "TIME")
-    header:SetAttribute("sortDirection", "-")
-    header:SetAttribute("wrapAfter", buffsperrow)
-
-    header:SetAttribute("minWidth", buffsperrow == 1 and 0 or spacing + buttonsize * (buffsperrow))
-    header:SetAttribute("minHeight", buttonsize)
-    header:SetAttribute("wrapYOffset", -(buttonsize + spacing))
-    AurasHolder:SetWidth(header:GetAttribute("minWidth"))
-
+function Module:UpdateHeader(header)
+    if (not C["Auras"].Enable) then return end
     if header:GetAttribute("filter") == "HELPFUL" then
-        header:SetAttribute("separateOwn", 1)
-        header:SetAttribute("maxWraps", 3)
-        header:SetAttribute("minHeight", buttonsize* 3 + spacing* 2)
-        header:SetAttribute("weaponTemplate", ("KkthnxUIAuraTemplate%d"):format(buttonsize))
+        header:SetAttribute("consolidateTo", 0)
+        header:SetAttribute("weaponTemplate", ("KkthnxUIAuraTemplate%d"):format(C["Auras"].size))
     end
 
-    header:SetAttribute("template", ("KkthnxUIAuraTemplate%d"):format(buttonsize))
-    A:PostDrag()
+    header:SetAttribute("separateOwn", C["Auras"].SeperateOwn)
+    header:SetAttribute("sortMethod", C["Auras"].SortMethod.Value)
+    header:SetAttribute("sortDirection", C["Auras"].SortDir.Value)
+    header:SetAttribute("maxWraps", C["Auras"].MaxWraps)
+    header:SetAttribute("wrapAfter", C["Auras"].WrapAfter)
+
+    header:SetAttribute("point", DIRECTION_TO_POINT[C["Auras"].GrowthDirection.Value])
+
+    if (IS_HORIZONTAL_GROWTH[C["Auras"].GrowthDirection.Value]) then
+        header:SetAttribute("minWidth", ((C["Auras"].WrapAfter == 1 and 0 or C["Auras"].HorizontalSpacing) + C["Auras"].Size) * C["Auras"].WrapAfter)
+        header:SetAttribute("minHeight", (C["Auras"].VerticalSpacing + C["Auras"].Size) * C["Auras"].MaxWraps)
+        header:SetAttribute("xOffset", DIRECTION_TO_HORIZONTAL_SPACING_MULTIPLIER[C["Auras"].GrowthDirection.Value] * (C["Auras"].HorizontalSpacing + C["Auras"].Size))
+        header:SetAttribute("yOffset", 0)
+        header:SetAttribute("wrapXOffset", 0)
+        header:SetAttribute("wrapYOffset", DIRECTION_TO_VERTICAL_SPACING_MULTIPLIER[C["Auras"].GrowthDirection.Value] * (C["Auras"].VerticalSpacing + C["Auras"].Size))
+    else
+        header:SetAttribute("minWidth", (C["Auras"].HorizontalSpacing + C["Auras"].Size) * C["Auras"].MaxWraps)
+        header:SetAttribute("minHeight", ((C["Auras"].WrapAfter == 1 and 0 or C["Auras"].VerticalSpacing) + C["Auras"].Size) * C["Auras"].WrapAfter)
+        header:SetAttribute("xOffset", 0)
+        header:SetAttribute("yOffset", DIRECTION_TO_VERTICAL_SPACING_MULTIPLIER[C["Auras"].GrowthDirection.Value] * (C["Auras"].VerticalSpacing + C["Auras"].Size))
+        header:SetAttribute("wrapXOffset", DIRECTION_TO_HORIZONTAL_SPACING_MULTIPLIER[C["Auras"].GrowthDirection.Value] * (C["Auras"].HorizontalSpacing + C["Auras"].Size))
+        header:SetAttribute("wrapYOffset", 0)
+    end
+
+    header:SetAttribute("template", ("KkthnxUIAuraTemplate%d"):format(C["Auras"].Size))
+    local index = 1
+    local child = select(index, header:GetChildren())
+    while(child) do
+        if ((floor(child:GetWidth() * 100 + 0.5) / 100) ~= C["Auras"].Size) then
+            child:SetSize(C["Auras"].Size, C["Auras"].Size)
+        end
+
+        if (child.time) then
+            child.time:ClearAllPoints()
+            child.time:SetPoint("TOP", child, "BOTTOM", 1, -4)
+            child.time:FontTemplate(nil, 12, "OUTLINE")
+
+            child.count:ClearAllPoints()
+            child.count:SetPoint("BOTTOMRIGHT", -1, 0)
+            child.count:FontTemplate(nil, 12, "OUTLINE")
+        end
+
+        -- Blizzard bug fix, icons arent being hidden when you reduce the amount of maximum buttons
+        if (index > (C["Auras"].MaxWraps * C["Auras"].WrapAfter) and child:IsShown()) then
+            child:Hide()
+        end
+
+        index = index + 1
+        child = select(index, header:GetChildren())
+    end
 end
 
-function A:CreateAuraHeader(filter)
-    local name
-    if filter == "HELPFUL" then name = "KkthnxUIPlayerBuffs" else name = "KkthnxUIPlayerDebuffs" end
+function Module:CreateAuraHeader(filter)
+    local name = "KkthnxUIPlayerDebuffs"
+    if filter == "HELPFUL" then
+        name = "KkthnxUIPlayerBuffs"
+    end
 
     local header = CreateFrame("Frame", name, UIParent, "SecureAuraHeaderTemplate")
     header:SetClampedToScreen(true)
-    header:SetAttribute("template", "KkthnxUIAuraTemplate30")
     header:SetAttribute("unit", "player")
     header:SetAttribute("filter", filter)
     RegisterStateDriver(header, "visibility", "[petbattle] hide; show")
+    RegisterAttributeDriver(header, "unit", "[vehicleui] vehicle; player")
 
     if filter == "HELPFUL" then
         header:SetAttribute("consolidateDuration", -1)
         header:SetAttribute("includeWeapons", 1)
     end
 
-    A:UpdateHeader(header)
+    Module:UpdateHeader(header)
     header:Show()
 
     return header
 end
 
-function A:PostDrag(position)
-    if InCombatLockdown() then return end
-    local headers = {KkthnxUIPlayerBuffs, KkthnxUIPlayerDebuffs}
-    for _, header in pairs(headers) do
-        if header then
-            if not position then position = K.GetScreenQuadrant(header) end
-            if string.find(position, "LEFT") then
-                header:SetAttribute("point", "TOPLEFT")
-                header:SetAttribute("xOffset", buttonsize + spacing)
-            else
-                header:SetAttribute("point", "TOPRIGHT")
-                header:SetAttribute("xOffset", -(buttonsize + spacing))
-            end
+function Module:OnInitialize()
+    if (not C["Auras"].Enable) then return end
 
-            header:ClearAllPoints()
-        end
-    end
-
-    if string.find(position, "LEFT") then
-        KkthnxUIPlayerBuffs:SetPoint("TOPLEFT", AurasHolder, "TOPLEFT", 0, 0)
-
-        if KkthnxUIPlayerDebuffs then
-            KkthnxUIPlayerDebuffs:SetPoint("BOTTOMLEFT", KkthnxUIPlayerBuffs, "BOTTOMLEFT", 0,0)
-        end
-    else
-        KkthnxUIPlayerBuffs:SetPoint("TOPRIGHT", AurasHolder, "TOPRIGHT", 0, 0)
-
-        if KkthnxUIPlayerDebuffs then
-            KkthnxUIPlayerDebuffs:SetPoint("BOTTOMRIGHT", AurasHolder, "BOTTOMRIGHT", 0, 0)
-        end
-    end
-end
-
-function A:OnEnable()
-    if C["Auras"].Enable ~= true then return end
-
-    local AurasHolder = CreateFrame("Frame", "AurasHolder", UIParent)
-    AurasHolder:SetHeight(K.Scale(buttonsize)* 4 + K.Scale(spacing)* 3)
-    AurasHolder:SetWidth(K.Scale(buttonsize)* buffsperrow + K.Scale(spacing)* (buffsperrow-1))
-    AurasHolder:SetFrameStrata("BACKGROUND")
-    AurasHolder:SetClampedToScreen(true)
-    AurasHolder:SetAlpha(0)
+    local AurasHolder = CreateFrame("Frame", "AurasHolder", Minimap)
     if C["Minimap"].CollectButtons then
       AurasHolder:SetPoint(C.Position.PlayerBuffs[1], C.Position.PlayerBuffs[2], C.Position.PlayerBuffs[3], C.Position.PlayerBuffs[4], C.Position.PlayerBuffs[5])
     else
-      AurasHolder:SetPoint("TOPRIGHT", "Minimap", "TOPLEFT", -8, 0)
+      AurasHolder:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -3, 3)
     end
-    K.Movers:RegisterFrame(AurasHolder)
+    AurasHolder:SetWidth((Minimap:GetWidth() + 29))
+    AurasHolder:SetHeight(Minimap:GetHeight() + 53)
 
     self.BuffFrame = self:CreateAuraHeader("HELPFUL")
-    self.DebuffFrame = self:CreateAuraHeader("HARMFUL")
+    self.BuffFrame:SetPoint("TOPRIGHT", AurasHolder, "TOPLEFT", -(6 + 4), -4 - 4)
+    K["Movers"]:RegisterFrame(self.BuffFrame)
 
-    self.AlphaFrame = CreateFrame("Frame")
-    self.AlphaFrame.BuffAlphaValue = 1
-    self.AlphaFrame.BuffFrameFlashState = 1
-    self.AlphaFrame.BuffFrameFlashTime = 0
-    self.AlphaFrame:SetScript("OnUpdate", A.UpdateAlpha)
+    self.DebuffFrame = self:CreateAuraHeader("HARMFUL")
+    self.DebuffFrame:SetPoint("BOTTOMRIGHT", AurasHolder, "BOTTOMLEFT", -(6 + 4), -4 - 93)
+    K["Movers"]:RegisterFrame(self.DebuffFrame)
 end
