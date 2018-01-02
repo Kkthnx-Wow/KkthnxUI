7a552e7e628ef74584e3a457e1a207308f0bc6b9
Kkthnx <JRussell20@gmail.com>
2018-01-02 02:10:40 -0500

    Just some comment info on how we import our engine in another addon.

--- KkthnxUI/Init.lua
+++ KkthnxUI/Init.lua
@@ -1,24 +1,34 @@
 debugprofilestart()
 
---[[The MIT License (MIT)
+--[[
+	The MIT License (MIT)
 
-Copyright (c) 2012 - 2017 Kkthnx (Joshua Russell) kkthnxui@gmail.com
+	Copyright (c) 2012 - 2018 Kkthnx (Joshua Russell) kkthnxui@gmail.com
 
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the "Software"),
-to deal in the Software without restriction, including without limitation the
-rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is furnished
-to do so, subject to the following conditions:
+	Permission is hereby granted, free of charge, to any person obtaining
+	a copy of this software and associated documentation files (the "Software"),
+	to deal in the Software without restriction, including without limitation the
+	rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is furnished
+	to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
-ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.--]]
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+	INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+	PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+	ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+	ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+]]
+
+--[[
+	This should be at the top of every file inside of the KkthnxUI AddOn:
+	local K, C, L = unpack(select(2, ...))
+
+	This is how another addon imports the KkthnxUI engine:
+	local K, C, L = unpack(KkthnxUI)
+]]
 
 local AddOnName, Engine = ...
 
@@ -71,10 +81,6 @@ local AddOn = LibStub("AceAddon-3.0"):NewAddon(AddOnName, "AceConsole-3.0", "Ace
 Engine[1] = AddOn
 Engine[2] = {}
 Engine[3] = {}
-
-function Engine:unpack()
-	return self[1], self[2], self[3], self[4]
-end
 
 _G[AddOnName] = Engine
 
