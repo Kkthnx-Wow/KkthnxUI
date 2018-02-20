cc7a11b2a3406926285dfe020f4401d250919b95
Kkthnx <JRussell20@gmail.com>
2018-02-20 15:54:15 -0500

    Locale Updates.

--- KkthnxUI/Locales/enUS.lua
+++ KkthnxUI/Locales/enUS.lua
@@ -17,7 +17,7 @@ L["Install"] = {
 	Step_0 = "Thank you for choosing |cff4488ffKkthnxUI|r!|n|nYou will be guided through the installation process in a few simple steps. At each step you can decide whether or not you want to apply or skip the presented settings.",
 	Step_1 = "The first step applies the essential settings. This is |cffff0000recommended|r for any user unless you want to apply only a specific part of the settings.|n|nClick 'Apply' to apply the settings and 'Next' to continue the install process. If you wish to skip this step just press 'Next'.",
 	Step_2 = "The second step applies the correct chat setup. If you are a new user this step is recommended. If you are an existing user you may want to skip this step.|n|nClick 'Apply' to apply the settings and 'Next' to continue the install process. If you wish to skip this step just press 'Next'.",
-	Step_3 = "Installation is complete. Please click the 'Complete' button to reload the UI. Enjoy KkthnxUI!\n\nVisit us at\n\n|cff7289DADiscord:|r YUmxqQm\n|cff3b5998Facebook:|r @kkthnxui\n|cff00acedTwitter:|r @kkthnxui",
+	Step_3 = "Installation is complete. Please click the 'Complete' button to reload the UI. Enjoy KkthnxUI!|n|nVisit us at|n|n|cff7289DADiscord:|r YUmxqQm|n|cff3b5998Facebook:|r @kkthnxui|n|cff00acedTwitter:|r @kkthnxui",
 	Welcome_1 = "Welcome to |cff4488ffKkthnxUI|r v",
 	Welcome_2 = "Type |cffffbb44/cfg|r to access the in-game configuration menu.",
 	Welcome_3 = "If you are in need of support you can visit our Discord |cffffbb44YUmxqQm|r",
@@ -27,7 +27,7 @@ L["Install"] = {
 L["StaticPopups"] = {
 	Changes_Reload = "One or more of the changes you have made require a ReloadUI.",
 	Config_Reload = "One or more of the changes you have made require a ReloadUI.",
-	Delete_Grays = "|cffff2020WARNING!|r\n\nYou are about to delete all your gray items. You will not receive any currency for this. Do you want to continue?\n\nThe net worth of items being deleted displayed below.",
+	Delete_Grays = "|cffff2020WARNING!|r|n|nYou are about to delete all your gray items. You will not receive any currency for this. Do you want to continue?|n|nThe net worth of items being deleted displayed below.",
 	Disband_Group = "Are you sure you want to disband the group?",
 	Fix_Actionbars = "There seems to be an issue with your actionbars. Would you like to attempt to fix the issue?",
 	KkthnxUI_Update = "KkthnxUI is out of date. You can download the newest version from Curse!",
@@ -195,9 +195,9 @@ L["DataText"] = {
 -- Inventory Localization
 L["Inventory"] = {
 	Artifact_Count = "Count: ",
-	Artifact_Use = "Right click to use",
-	Buttons_Artifact = "Right click to use Artifact Power item in bag",
-	Buttons_Sort = "Left Click: Sort \nRight Click: Blizzard Sort",
+	Artifact_Use = "|cff02FF02|nRight click to use|r",
+	--Buttons_Artifact = "|cff02FF02|nRight click to use Artifact Power item in bag|r",
+	Buttons_Sort = "Left Click: Sort |nRight Click: Blizzard Sort",
 	Buttons_Stack = "Stack Items",
 	Cant_Buy_Slot = "Can't buy anymore slots!",
 	NotEnoughMoney = "You don't have enough money to repair!",
@@ -207,7 +207,7 @@ L["Inventory"] = {
 	Shift_Move = "Hold Shift + Drag",
 	Show_Bags = "Toggle Bags",
 	SoldTrash = "Your vendor trash has been sold and you earned",
-	TrashList = "\n\nTrash List:\n",
+	TrashList = "|n|nTrash List:|n",
 }
 
 -- Loot Localization
@@ -219,9 +219,9 @@ L["Loot"] = {
 -- Maps Localization
 L["Maps"] = {
 	DisableToHide = "Disable to hide areas|nyou have not yet discovered.",
-	EnableToShow = "Enable to show hidden area|nyou have not yet discovered.",
+	EnableToShow = "Enable to show hidden areas|nyou have not yet discovered.",
 	HideUnDiscovered = "Hide Undiscovered Areas",
-	PressToCopy = "Press CTRL/C to copy.",
+	PressToCopy = "|nPress <CTRL/C> to copy.",
 	Reveal = "Reveal",
 	RevealHidden = "Reveal Hidden Areas",
 }
