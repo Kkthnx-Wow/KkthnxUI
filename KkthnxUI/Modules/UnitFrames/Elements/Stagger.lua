89c0c18d0e68c0ae2665130cdc15cb8887ca83be
Kkthnx <JRussell20@gmail.com>
2018-03-09 16:54:09 -0500

    Fix Staggerbar for brew monks.

--- KkthnxUI/Modules/UnitFrames/Elements/Stagger.lua
+++ KkthnxUI/Modules/UnitFrames/Elements/Stagger.lua
@@ -5,14 +5,13 @@ local UnitframeFont = K.GetFont(C["Unitframe"].Font)
 local UnitframeTexture = K.GetTexture(C["Unitframe"].Texture)
 
 function K.CreateStagger(self)
-	if K.Class ~= "MONK" then return end
-
 	local stagger = CreateFrame("StatusBar", nil, self)
-	stagger:SetPoint("LEFT")
-	stagger:SetPoint("RIGHT")
-	stagger:SetPoint("BOTTOM", self, "TOP", 0, 0)
+	stagger:SetPoint("LEFT", 4, 0)
+	stagger:SetPoint("RIGHT", -4, 0)
+	stagger:SetPoint("BOTTOM", self, "TOP", 0, 3)
 	stagger:SetHeight(12)
 	stagger:SetStatusBarTexture(UnitframeTexture)
+	stagger:SetTemplate("Transparent")
 
 	self.Stagger = stagger
 end
