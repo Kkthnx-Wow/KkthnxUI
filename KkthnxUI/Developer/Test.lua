d6677bb107924e56caf3ce43bf9856b87f975bf2
Kkthnx <JRussell20@gmail.com>
2018-04-04 04:45:32 -0400

    I only want to test this. Not to be public.

--- KkthnxUI/Developer/Test.lua
+++ KkthnxUI/Developer/Test.lua
@@ -1,12 +1,15 @@
 local K, C, L = unpack(select(2, ...))
+if not K.Realm == "Felsong" then
+  return
+end
 
 local _G = _G
 
--- local COOLDOWN_TYPE_LOSS_OF_CONTROL = _G.COOLDOWN_TYPE_LOSS_OF_CONTROL
--- local hooksecurefunc = _G.hooksecurefunc
+local COOLDOWN_TYPE_LOSS_OF_CONTROL = _G.COOLDOWN_TYPE_LOSS_OF_CONTROL
+local hooksecurefunc = _G.hooksecurefunc
 
--- hooksecurefunc("CooldownFrame_Set", function(self)
---   if self.currentCooldownType == COOLDOWN_TYPE_LOSS_OF_CONTROL then
---     self:SetCooldown(0, 0)
---   end
--- end)
+hooksecurefunc("CooldownFrame_Set", function(self)
+  if self.currentCooldownType == COOLDOWN_TYPE_LOSS_OF_CONTROL then
+    self:SetCooldown(0, 0)
+  end
+end)
