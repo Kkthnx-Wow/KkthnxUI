4727f133184f42bea1abd3ef14873d9e1b84b079
Kkthnx <JRussell20@gmail.com>
2018-08-13 04:52:13 -0400

    Rewrite AutoCollapse

--- KkthnxUI/Modules/Quests/AutoCollapse.lua
+++ KkthnxUI/Modules/Quests/AutoCollapse.lua
@@ -1,37 +1,42 @@
-local K, C, L = unpack(select(2, ...))
-if C["Quests"].AutoCollapse ~= true then return end
+local K, C = unpack(select(2, ...))
+local Module = K:NewModule("AutoCollapse", "AceEvent-3.0")
 
--- Sourced: ElvUI Shadow & Light (Darth_Predator, Repooc)
-
--- Wow Lua
 local _G = _G
 
--- Wow API
 local InCombatLockdown = _G.InCombatLockdown
 local IsInInstance = _G.IsInInstance
-local ObjectiveTracker_Expand, ObjectiveTracker_Collapse = _G.ObjectiveTracker_Expand, _G.ObjectiveTracker_Collapse
+local ObjectiveTracker_Collapse = _G.ObjectiveTracker_Collapse
+local ObjectiveTracker_Expand = _G.ObjectiveTracker_Expand
 
+local trackerFrame = _G["ObjectiveTrackerFrame"]
 local minimizeButton = _G["ObjectiveTrackerFrame"].HeaderMenu.MinimizeButton
-
-local AutoCollapse = CreateFrame("Frame")
-AutoCollapse:RegisterEvent("LOADING_SCREEN_DISABLED")
-AutoCollapse:RegisterEvent("ZONE_CHANGED_NEW_AREA")
-AutoCollapse:RegisterEvent("PLAYER_REGEN_ENABLED")
-AutoCollapse:RegisterEvent("PLAYER_REGEN_DISABLED")
-AutoCollapse:SetScript("OnEvent", function(self, event)
+function Module:ChangeState(event)
 	if InCombatLockdown() and event ~= "PLAYER_REGEN_DISABLED" then
 		return
 	end
 
-	local instance, instanceType = IsInInstance()
-
-	if instance then
-		if instanceType == "party" or instanceType == "scenario" or instanceType == "raid" or instanceType == "pvp" or instanceType == "arena" then
-			ObjectiveTracker_Collapse()
-			minimizeButton:SetNormalTexture("Interface\\AddOns\\KkthnxUI\\Media\\Textures\\TrackerButton")
-		elseif ObjectiveTrackerFrame.collapsed and not InCombatLockdown() then
-			ObjectiveTracker_Expand()
-			minimizeButton:SetNormalTexture("Interface\\AddOns\\KkthnxUI\\Media\\Textures\\TrackerButton")
-		end
+	if (not IsInInstance() and trackerFrame.collapsed) then
+		ObjectiveTracker_Expand()
+		minimizeButton:SetNormalTexture("Interface\\AddOns\\KkthnxUI\\Media\\Textures\\TrackerButton")
+		return
 	end
-end)
+
+	if (IsInInstance() and not trackerFrame.collapsed) then
+		ObjectiveTracker_Collapse()
+		minimizeButton:SetNormalTexture("Interface\\AddOns\\KkthnxUI\\Media\\Textures\\TrackerButton")
+	elseif (trackerFrame.collapsed) then
+		ObjectiveTracker_Expand()
+		minimizeButton:SetNormalTexture("Interface\\AddOns\\KkthnxUI\\Media\\Textures\\TrackerButton")
+	end
+end
+
+function Module:OnEnable()
+	if C["Quests"].AutoCollapse ~= true then
+		return
+	end
+
+	self:RegisterEvent("LOADING_SCREEN_DISABLED", "ChangeState")
+	self:RegisterEvent("ZONE_CHANGED_NEW_AREA", "ChangeState")
+	self:RegisterEvent("PLAYER_REGEN_ENABLED", "ChangeState")
+	self:RegisterEvent("PLAYER_REGEN_DISABLED", "ChangeState")
+end
