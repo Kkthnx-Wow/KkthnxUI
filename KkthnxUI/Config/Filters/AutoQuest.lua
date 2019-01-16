c1ce7348b1887968e9a4a57e96ac46818a5d73b3
Kkthnx <JRussell20@gmail.com>
2019-01-16 16:09:45 -0500

    I seriously still messed this up.

---
+++ KkthnxUI/Config/Filters/AutoQuest.lua
@@ -0,0 +1,136 @@
+local K, C = unpack(select(2, ...))
+if C["Automation"].AutoQuest ~= true then
+	return
+end
+
+K.AutoQuestBlacklistDB  = {
+	items = {
+		-- Inscription weapons
+		[31690] = 79343, -- Inscribed Tiger Staff
+		[31691] = 79340, -- Inscribed Crane Staff
+		[31692] = 79341, -- Inscribed Serpent Staff
+
+		-- Darkmoon Faire artifacts
+		[29443] = 71635, -- Imbued Crystal
+		[29444] = 71636, -- Monstrous Egg
+		[29445] = 71637, -- Mysterious Grimoire
+		[29446] = 71638, -- Ornate Weapon
+		[29451] = 71715, -- A Treatise on Strategy
+		[29456] = 71951, -- Banner of the Fallen
+		[29457] = 71952, -- Captured Insignia
+		[29458] = 71953, -- Fallen Adventurer's Journal
+		[29464] = 71716, -- Soothsayer's Runes
+
+		-- Tiller Gifts
+		["progress_79264"] = 79264, -- Ruby Shard
+		["progress_79265"] = 79265, -- Blue Feather
+		["progress_79266"] = 79266, -- Jade Cat
+		["progress_79267"] = 79267, -- Lovely Apple
+		["progress_79268"] = 79268, -- Marsh Lily
+
+		-- Garrison scouting missives
+		[38176] = 122405, -- Scouting Missive: Stonefury Cliffs
+		[38177] = 122403, -- Scouting Missive: Magnarok
+		[38178] = 122402, -- Scouting Missive: Iron Siegeworks
+		[38179] = 122400, -- Scouting Missive: Everbloom Wilds
+		[38180] = 122424, -- Scouting Missive: Broken Precipice
+		[38181] = 122421, -- Scouting Missive: Mok'gol Watchpost
+		[38182] = 122418, -- Scouting Missive: Darktide Roost
+		[38183] = 122416, -- Scouting Missive: Socrethar's Rise
+		[38184] = 122413, -- Scouting Missive: Lost Veil Anzu
+		[38185] = 122411, -- Scouting Missive: Pillars of Fate
+		[38186] = 122408, -- Scouting Missive: Skettis
+		[38187] = 122412, -- Scouting Missive: Shattrath Harbor
+		[38189] = 122401, -- Scouting Missive: Stonefury Cliffs
+		[38190] = 122399, -- Scouting Missive: Magnarok
+		[38191] = 122406, -- Scouting Missive: Iron Siegeworks
+		[38192] = 122404, -- Scouting Missive: Everbloom Wilds
+		[38193] = 122423, -- Scouting Missive: Broken Precipice
+		[38194] = 122420, -- Scouting Missive: Gorian Proving Grounds
+		[38195] = 122422, -- Scouting Missive: Mok'gol Watchpost
+		[38196] = 122417, -- Scouting Missive: Darktide Roost
+		[38197] = 122415, -- Scouting Missive: Socrethar's Rise
+		[38198] = 122414, -- Scouting Missive: Lost Veil Anzu
+		[38199] = 122409, -- Scouting Missive: Pillars of Fate
+		[38200] = 122407, -- Scouting Missive: Skettis
+		[38201] = 122410, -- Scouting Missive: Shattrath Harbor
+		[38202] = 122419, -- Scouting Missive: Gorian Proving Grounds
+
+		-- Misc
+		[31664] = 88604, -- Nat's Fishing Journal
+	}
+}
+
+K.IgnoreQuestNPC = {
+	[103792] = true, -- Griftah (one of his quests is a scam)
+	[111243] = true, -- Archmage Lan"dalock
+	[119388] = true, -- Chieftain Hatuun (repeatable resource quest)
+	[124312] = true, -- High Exarch Turalyon (repeatable resource quest)
+	[126954] = true, -- High Exarch Turalyon (repeatable resource quest)
+	[127037] = true, -- Nabiru (repeatable resource quest)
+	[141584] = true, -- Zurvan (Seal of Wartorn Fate, Horde)
+	[142063] = true, -- Tezran (Seal of Wartorn Fate, Alliance)
+	[87391] = true, -- Fate-Twister Seress
+	[88570] = true, -- Fate-Twister Tiklal
+}
+
+K.IgnoreGossipNPC = {
+	-- Bodyguards
+	[86682] = true, -- Tormmok
+	[86927] = true, -- Delvar Ironfist (Alliance)
+	[86933] = true, -- Vivianne (Horde)
+	[86934] = true, -- Defender Illona (Alliance)
+	[86945] = true, -- Aeda Brightdawn (Horde)
+	[86946] = true, -- Talonpriest Ishaal
+	[86964] = true, -- Leorajh
+
+	-- Sassy Imps
+	[95139] = true,
+	[95141] = true,
+	[95142] = true,
+	[95143] = true,
+	[95144] = true,
+	[95145] = true,
+	[95146] = true,
+	[95200] = true,
+	[95201] = true,
+
+	-- Misc NPCs
+	[117871] = true, -- War Councilor Victoria (Class Challenges @ Broken Shore)
+}
+
+K.RogueClassHallInsignia = {
+	[93188] = true, -- Mongar
+	[96782] = true, -- Lucian Trias
+	[97004] = true, -- "Red" Jack Findle
+}
+
+K.DarkmoonDailyNPCs = {
+	[14841] = true, -- Rinling
+	[15303] = true, -- Maxima Blastenheimer
+	[54485] = true, -- Jessica Rogers
+	[54601] = true, -- Mola
+	[54605] = true, -- Finlay Coolshot
+	[67370] = true, -- Jeremy Feasel
+	[85519] = true, -- Christoph VonFeasel
+	[85546] = true, -- Ziggie Sparks
+}
+
+K.DarkmoonNPC = {
+	[54334] = true, -- Darkmoon Faire Mystic Mage (Alliance)
+	[55382] = true, -- Darkmoon Faire Mystic Mage (Horde)
+	[57850] = true, -- Teleportologist Fozlebub
+}
+
+K.CashRewards = {
+	[45724] = 1e5, -- Champion"s Purse, 10 gold
+	[64491] = 2e6, -- Royal Reward, 200 gold
+
+	-- Items from the Sixtrigger brothers quest chain in Stormheim
+	[138127] = 15, -- Mysterious Coin, 15 copper
+	[138129] = 11, -- Swatch of Priceless Silk, 11 copper
+	[138131] = 24, -- Magical Sprouting Beans, 24 copper
+	[138123] = 15, -- Shiny Gold Nugget, 15 copper
+	[138125] = 16, -- Crystal Clear Gemstone, 16 copper
+	[138133] = 27, -- Elixir of Endless Wonder, 27 copper
+}
---
+++ KkthnxUI/Modules/Automation/AutoQuest.lua
@@ -0,0 +1,390 @@
+local K, C = unpack(select(2, ...))
+if C["Automation"].AutoQuest ~= true then
+	return
+end
+
+local _G = _G
+local next = next
+local select = select
+local string_find = string.find
+local string_match = string.match
+local string_sub = string.sub
+local tonumber = tonumber
+
+local AcceptQuest = _G.AcceptQuest
+local AcknowledgeAutoAcceptQuest = _G.AcknowledgeAutoAcceptQuest
+local C_Timer_After = _G.C_Timer.After
+local CompleteQuest = _G.CompleteQuest
+local GetActiveTitle = _G.GetActiveTitle
+local GetAutoQuestPopUp = _G.GetAutoQuestPopUp
+local GetAvailableQuestInfo = _G.GetAvailableQuestInfo
+local GetGossipActiveQuests = _G.GetGossipActiveQuests
+local GetGossipAvailableQuests = _G.GetGossipAvailableQuests
+local GetGossipOptions = _G.GetGossipOptions
+local GetInstanceInfo = _G.GetInstanceInfo
+local GetItemInfo = _G.GetItemInfo
+local GetItemInfoFromHyperlink = _G.GetItemInfoFromHyperlink
+local GetNumActiveQuests = _G.GetNumActiveQuests
+local GetNumAutoQuestPopUps = _G.GetNumAutoQuestPopUps
+local GetNumAvailableQuests = _G.GetNumAvailableQuests
+local GetNumGossipActiveQuests = _G.GetNumGossipActiveQuests
+local GetNumGossipAvailableQuests = _G.GetNumGossipAvailableQuests
+local GetNumGossipOptions = _G.GetNumGossipOptions
+local GetNumGroupMembers = _G.GetNumGroupMembers
+local GetNumQuestChoices = _G.GetNumQuestChoices
+local GetNumQuestItems = _G.GetNumQuestItems
+local GetNumQuestLogEntries = _G.GetNumQuestLogEntries
+local GetNumTrackingTypes = _G.GetNumTrackingTypes
+local GetQuestID = _G.GetQuestID
+local GetQuestItemInfo = _G.GetQuestItemInfo
+local GetQuestItemLink = _G.GetQuestItemLink
+local GetQuestLogIndexByID = _G.GetQuestLogIndexByID
+local GetQuestLogTitle = _G.GetQuestLogTitle
+local GetQuestReward = _G.GetQuestReward
+local GetQuestTagInfo = _G.GetQuestTagInfo
+local GetTrackingInfo = _G.GetTrackingInfo
+local IsQuestCompletable = _G.IsQuestCompletable
+local QuestFrame_OnEvent = _G.QuestFrame_OnEvent
+local QuestGetAutoAccept = _G.QuestGetAutoAccept
+local QuestInfoItem_OnClick = _G.QuestInfoItem_OnClick
+local QuestIsFromAreaTrigger = _G.QuestIsFromAreaTrigger
+local SelectActiveQuest = _G.SelectActiveQuest
+local SelectAvailableQuest = _G.SelectAvailableQuest
+local SelectGossipActiveQuest = _G.SelectGossipActiveQuest
+local SelectGossipAvailableQuest = _G.SelectGossipAvailableQuest
+local SelectGossipOption = _G.SelectGossipOption
+local SelectGossipOption = _G.SelectGossipOption
+local ShowQuestComplete = _G.ShowQuestComplete
+local ShowQuestOffer = _G.ShowQuestOffer
+local StaticPopup_Hide = _G.StaticPopup_Hide
+local UnitGUID = _G.UnitGUID
+local UnitIsDeadOrGhost = _G.UnitIsDeadOrGhost
+
+local AutoQuestDB = {
+	["FairePort"] = true,
+	["Gossip"] = true,
+	["GossipRaid"] = 1,
+	["Items"] = true,
+	["Modifier"] = "SHIFT",
+	["Nomi"] = true,
+	["Reverse"] = false,
+	["Share"] = false,
+	["Withered"] = true,
+}
+
+local AutoQuest = CreateFrame("Frame")
+AutoQuest:SetScript("OnEvent", function(self, event, ...)
+	self[event](...)
+end)
+
+local metatable = {
+	__call = function(methods, ...)
+		for _, method in next, methods do
+			method(...)
+		end
+	end
+}
+
+local modifier = false
+function AutoQuest:Register(event, method, override)
+	local newmethod
+	if (not override) then
+		newmethod = function(...)
+			if (AutoQuestDB.Reverse == modifier) then
+				method(...)
+			end
+		end
+	end
+
+	local methods = self[event]
+	if (methods) then
+		self[event] = setmetatable({methods, newmethod or method}, metatable)
+	else
+		self[event] = newmethod or method
+		self:RegisterEvent(event)
+	end
+end
+
+local function GetNPCID()
+	return tonumber(string_match(UnitGUID("npc") or "", "Creature%-.-%-.-%-.-%-.-%-(.-)%-"))
+end
+
+local function IsTrackingHidden()
+	for index = 1, GetNumTrackingTypes() do
+		local name, _, active = GetTrackingInfo(index)
+		if (name == (MINIMAP_TRACKING_TRIVIAL_QUESTS or MINIMAP_TRACKING_HIDDEN_QUESTS)) then
+			return active
+		end
+	end
+end
+
+local function GetQuestLogQuests(onlyComplete)
+	local quests = {}
+	for index = 1, GetNumQuestLogEntries() do
+		local title, _, _, isHeader, _, isComplete, _, questID = GetQuestLogTitle(index)
+		if (not isHeader) then
+			if (onlyComplete and isComplete or not onlyComplete) then
+				quests[title] = questID
+			end
+		end
+	end
+
+	return quests
+end
+
+AutoQuest:Register("QUEST_GREETING", function()
+	local npcID = GetNPCID()
+	if (K.IgnoreQuestNPC[npcID]) then
+		return
+	end
+
+	local active = GetNumActiveQuests()
+	if (active > 0) then
+		local logQuests = GetQuestLogQuests(true)
+		for index = 1, active do
+			local name, complete = GetActiveTitle(index)
+			if (complete) then
+				local questID = logQuests[name]
+				if (not questID) then
+					SelectActiveQuest(index)
+				else
+					local _, _, worldQuest = GetQuestTagInfo(questID)
+					if (not worldQuest) then
+						SelectActiveQuest(index)
+					end
+				end
+			end
+		end
+	end
+
+	local available = GetNumAvailableQuests()
+	if (available > 0) then
+		for index = 1, available do
+			local isTrivial, _, _, _, isIgnored = GetAvailableQuestInfo(index)
+			if ((not isTrivial and not isIgnored) or IsTrackingHidden()) then
+				SelectAvailableQuest(index)
+			end
+		end
+	end
+end)
+
+local function GetAvailableGossipQuestInfo(index)
+	local name, level, isTrivial, frequency, isRepeatable, isLegendary, isIgnored = select(((index * 7) - 7) + 1, GetGossipAvailableQuests())
+	return name, level, isTrivial, isIgnored, isRepeatable, frequency == 2, frequency == 3, isLegendary
+end
+
+local function GetActiveGossipQuestInfo(index)
+	local name, level, isTrivial, isComplete, isLegendary, isIgnored = select(((index * 6) - 6) + 1, GetGossipActiveQuests())
+	return name, level, isTrivial, isIgnored, isComplete, isLegendary
+end
+
+AutoQuest:Register("GOSSIP_SHOW", function()
+	local npcID = GetNPCID()
+	if (K.IgnoreQuestNPC[npcID]) then
+		return
+	end
+
+	local active = GetNumGossipActiveQuests()
+	if (active > 0) then
+		local logQuests = GetQuestLogQuests(true)
+		for index = 1, active do
+			local name, _, _, _, completed = GetActiveGossipQuestInfo(index)
+			if (completed) then
+				local questID = logQuests[name]
+				if (not questID) then
+					SelectGossipActiveQuest(index)
+				else
+					local _, _, worldQuest = GetQuestTagInfo(questID)
+					if (not worldQuest) then
+						SelectGossipActiveQuest(index)
+					end
+				end
+			end
+		end
+	end
+
+	local available = GetNumGossipAvailableQuests()
+	if (available > 0) then
+		for index = 1, available do
+			local _, _, trivial, ignored = GetAvailableGossipQuestInfo(index)
+			if ((not trivial and not ignored) or IsTrackingHidden()) then
+				SelectGossipAvailableQuest(index)
+			elseif (trivial and npcID == 64337 and AutoQuestDB.Nomi) then
+				SelectGossipAvailableQuest(index)
+			end
+		end
+	end
+
+	if (K.RogueClassHallInsignia[npcID]) then
+		return SelectGossipOption(1)
+	end
+
+	if (K.DarkmoonDailyNPCs[npcID] and active == 1 and not select(5, GetActiveGossipQuestInfo(1))) then
+		-- auto-start the daily interaction
+		for index = 1, GetNumGossipOptions() do
+			if (string_find((select((index * 2) - 1, GetGossipOptions())), "FF0008E8")) then
+				-- matching by the blue text color is sufficient
+				return SelectGossipOption(index)
+			end
+		end
+	end
+
+	if (available == 0 and active == 0 and GetNumGossipOptions() == 1) then
+		if (string_match((GetGossipOptions()), TRACKER_HEADER_PROVINGGROUNDS)) then
+			-- ignore proving grounds queue
+			return
+		end
+
+		if (AutoQuestDB.FairePort) then
+			if (npcID == 57850) then
+				return SelectGossipOption(1)
+			end
+		end
+
+		if (AutoQuestDB.Gossip) then
+			local _, instanceType, _, _, _, _, _, instanceMapID = GetInstanceInfo()
+			if (AutoQuestDB.Withered and instanceType == "scenario" and instanceMapID == 1626) then
+				return
+			end
+
+			if (instanceType == "raid" and AutoQuestDB.GossipRaid > 0) then
+				if (GetNumGroupMembers() > 1 and AutoQuestDB.GossipRaid < 2) then
+					return
+				end
+
+				SelectGossipOption(1)
+			elseif (instanceType ~= "raid" and not K.IgnoreGossipNPC[npcID]) then
+				SelectGossipOption(1)
+			end
+		end
+	end
+end)
+
+AutoQuest:Register("GOSSIP_CONFIRM", function(index)
+	if (not AutoQuestDB.FairePort) then return end
+
+	local npcID = GetNPCID()
+	if (npcID and K.DarkmoonNPC[npcID]) then
+		SelectGossipOption(index, "", true)
+		StaticPopup_Hide("GOSSIP_CONFIRM")
+	end
+end)
+
+QuestFrame:UnregisterEvent("QUEST_DETAIL")
+AutoQuest:Register("QUEST_DETAIL", function(...)
+	if (not QuestGetAutoAccept() and not QuestIsFromAreaTrigger() and not K.AutoQuestBlacklistDB[GetQuestID()]) then
+		QuestFrame_OnEvent(QuestFrame, "QUEST_DETAIL", ...)
+	end
+end, true)
+
+AutoQuest:Register("QUEST_DETAIL", function(questStartItemID)
+	if (QuestGetAutoAccept() or (questStartItemID ~= nil and questStartItemID ~= 0)) then
+		AcknowledgeAutoAcceptQuest()
+	else
+		-- XXX: no way to tell if the quest is trivial
+		AcceptQuest()
+	end
+end)
+
+local function AttemptAutoComplete(event)
+	if (GetNumAutoQuestPopUps() > 0) then
+		if (UnitIsDeadOrGhost("player")) then
+			AutoQuest:Register("PLAYER_REGEN_ENABLED", AttemptAutoComplete)
+			return
+		end
+
+		local questID, popUpType = GetAutoQuestPopUp(1)
+		if (popUpType == "OFFER") then
+			ShowQuestOffer(GetQuestLogIndexByID(questID))
+		else
+			ShowQuestComplete(GetQuestLogIndexByID(questID))
+		end
+	else
+		C_Timer_After(1, AttemptAutoComplete)
+	end
+
+	if (event == "PLAYER_REGEN_ENABLED") then
+		AutoQuest:UnregisterEvent("PLAYER_REGEN_ENABLED")
+	end
+end
+
+AutoQuest:Register("PLAYER_LOGIN", AttemptAutoComplete)
+AutoQuest:Register("QUEST_AUTOCOMPLETE", AttemptAutoComplete)
+AutoQuest:Register("QUEST_ACCEPT_CONFIRM", AcceptQuest)
+
+AutoQuest:Register("QUEST_ACCEPTED", function(id)
+	if (AutoQuestDB.Share) then
+		QuestLogPushQuest(id)
+	end
+end)
+
+local choiceQueue
+AutoQuest:Register("QUEST_ITEM_UPDATE", function()
+	if (choiceQueue and AutoQuest[choiceQueue]) then
+		AutoQuest[choiceQueue]()
+	end
+end, true)
+
+AutoQuest:Register("QUEST_PROGRESS", function()
+	if (IsQuestCompletable()) then
+		local requiredItems = GetNumQuestItems()
+		if (requiredItems > 0) then
+			for index = 1, requiredItems do
+				local link = GetQuestItemLink("required", index)
+				if (link) then
+					local id = GetItemInfoFromHyperlink(link)
+					for _, itemID in next, K.AutoQuestBlacklistDB.items do
+						if (itemID == id) then
+							return
+						end
+					end
+				else
+					choiceQueue = "QUEST_PROGRESS"
+					return
+				end
+			end
+		end
+
+		CompleteQuest()
+	end
+end)
+
+AutoQuest:Register("QUEST_COMPLETE", function()
+	local choices = GetNumQuestChoices()
+	if (choices <= 1) then
+		GetQuestReward(1)
+	end
+end)
+
+AutoQuest:Register("QUEST_COMPLETE", function()
+	local choices = GetNumQuestChoices()
+	if (choices > 1) then
+		local bestValue, bestIndex = 0
+
+		for index = 1, choices do
+			local link = GetQuestItemLink("choice", index)
+			if (link) then
+				local _, _, _, _, _, _, _, _, _, _, value = GetItemInfo(link)
+				value = K.CashRewards[(GetItemInfoFromHyperlink(link))] or value
+
+				if (value > bestValue) then
+					bestValue, bestIndex = value, index
+				end
+			else
+				choiceQueue = "QUEST_COMPLETE"
+				return GetQuestItemInfo("choice", index)
+			end
+		end
+
+		if (bestIndex) then
+			QuestInfoItem_OnClick(QuestInfoRewardsFrame.RewardButtons[bestIndex])
+		end
+	end
+end, true)
+
+local sub = string_sub
+AutoQuest:Register("MODIFIER_STATE_CHANGED", function(key, state)
+	if (sub(key, 2) == AutoQuestDB.Modifier) then
+		modifier = state == 1
+	end
+end, true)
