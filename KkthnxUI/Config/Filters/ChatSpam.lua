ee66b51ec87b46fb6213b0caed07dbf48b4cfbfc
Josh <JRussell20@gmail.com>
2019-03-05 00:19:18 -0500

    Can the whole damn file be pushed this time...?

--- KkthnxUI/Config/Filters/ChatSpam.lua
+++ KkthnxUI/Config/Filters/ChatSpam.lua
@@ -14,16 +14,9 @@ K.PrivateChatEventSpam = {
 	"%[(.*)Autobroadcast(.*)%]",
 	"%[(.*)BG Queue Announcer(.*)%]",
 
-	_G.ERR_LEARN_ABILITY_S:gsub("%%s","(.*)"),
-	_G.ERR_LEARN_PASSIVE_S:gsub("%%s","(.*)"),
-	_G.ERR_LEARN_SPELL_S:gsub("%%s","(.*)"),
 	_G.ERR_NOT_IN_INSTANCE_GROUP or "You aren't in an instance group.",
 	_G.ERR_NOT_IN_RAID or "You are not in a raid group",
-	_G.ERR_PET_LEARN_ABILITY_S:gsub("%%s","(.*)"),
-	_G.ERR_PET_LEARN_SPELL_S:gsub("%%s","(.*)"),
-	_G.ERR_PET_SPELL_UNLEARNED_S:gsub("%%s","(.*)"),
 	_G.ERR_QUEST_ALREADY_ON or "You are already on that quest",
-	_G.ERR_SPELL_UNLEARNED_S:gsub("%%s","(.*)"),
 }
 
 K.PrivateChatNoEventSpam = {
