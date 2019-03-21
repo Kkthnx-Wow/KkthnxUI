ac4f8e47e98b2b10addba8c85337d021a502afb4
Josh <JRussell20@gmail.com>
2019-03-21 08:40:02 -0400

    Update Battleground.lua This fixes #35

--- KkthnxUI/Modules/DataText/Battleground.lua
+++ KkthnxUI/Modules/DataText/Battleground.lua
@@ -1,130 +1,89 @@
 local K, C, L = unpack(select(2, ...))
-if C["DataText"].Battleground ~= true then return end
+local Module = CreateFrame("Frame", nil, UIParent)
 
 local _G = _G
-local select = select
 local string_format = string.format
 
-local CurrentMapID = _G.CurrentMapID
+local DAMAGE = _G.DAMAGE
+local DEATHS = _G.DEATHS
+local GameTooltip = _G.GameTooltip
 local GetBattlefieldScore = _G.GetBattlefieldScore
 local GetBattlefieldStatData = _G.GetBattlefieldStatData
-local GetCurrentMapAreaID = _G.GetCurrentMapAreaID
+local GetBattlefieldStatInfo = _G.GetBattlefieldStatInfo
 local GetNumBattlefieldScores = _G.GetNumBattlefieldScores
+local GetNumBattlefieldStats = _G.GetNumBattlefieldStats
+local HEALS = _G.HEALS
+local HONOR = _G.HONOR
+local HONORABLE_KILLS = _G.HONORABLE_KILLS
 local IsInInstance = _G.IsInInstance
+local KILLING_BLOWS = _G.KILLING_BLOWS
 local RAID_CLASS_COLORS = _G.RAID_CLASS_COLORS
-local RequestBattlefieldScoreData = _G.RequestBattlefieldScoreData
-local SetMapToCurrentZone = _G.SetMapToCurrentZone
-local UnitClass = _G.UnitClass
 
-local NameColor = K.RGBToHex(K.Color.r, K.Color.g, K.Color.b)
-local ValueColor = K.RGBToHex(1, 1, 1)
+Module.NameColor = K.RGBToHex(K.Color.r, K.Color.g, K.Color.b)
+Module.ValueColor = K.RGBToHex(1, 1, 1)
 
--- Map IDs
-local WSG = 443
-local TP = 626
-local AV = 401
-local SOTA = 512
-local IOC = 540
-local EOTS = 482
-local TBFG = 736
-local AB = 461
-local TOK = 856
-local SSM = 860
-local DG = 935
-local ASH = 978
-
-local DataTextBG = CreateFrame("Frame", nil, UIParent)
-DataTextBG:SetSize(300, 13)
-DataTextBG:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 0, -4)
-DataTextBG:SetFrameLevel(0)
-DataTextBG:SetFrameStrata("BACKGROUND")
-DataTextBG:EnableMouse(true)
-
-local function OnEnter(self)
+local int = 2
+function Module:OnEnter()
 	local NumScores = GetNumBattlefieldScores()
+	local NumExtraStats = GetNumBattlefieldStats()
 
 	for i = 1, NumScores do
 		local Name, KillingBlows, HonorableKills, Deaths, HonorGained, _, _, _, _, DamageDone, HealingDone = GetBattlefieldScore(i)
 
 		if (Name and Name == K.Name) then
-			local CurMapID = GetCurrentMapAreaID()
-			local Color = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[K.Class] or RAID_CLASS_COLORS[K.Class]
+			local Color = RAID_CLASS_COLORS[K.Class]
 			local ClassColor = string_format("|cff%.2x%.2x%.2x", Color.r * 255, Color.g * 255, Color.b * 255)
-			SetMapToCurrentZone()
-			GameTooltip:SetOwner(self, "ANCHOR_BOTTOMRIGHT")
-			GameTooltip:SetPoint("BOTTOMRIGHT", self, "BOTTOMRIGHT", -10, 0)
-			GameTooltip:ClearLines()
 
+			GameTooltip:SetOwner(self, "ANCHOR_TOPLEFT", 0, 4)
+			GameTooltip:ClearLines()
+			GameTooltip:SetPoint("BOTTOM", self, "TOP", 0, 1)
+			GameTooltip:ClearLines()
 			GameTooltip:AddDoubleLine(L["DataText"].StatsFor, ClassColor..Name.."|r")
 			GameTooltip:AddLine(" ")
-			GameTooltip:AddDoubleLine(L["DataText"].KillingBlow, KillingBlows, 1, 1, 1)
-			GameTooltip:AddDoubleLine(L["DataText"].HonorableKill, HonorableKills, 1, 1, 1)
-			GameTooltip:AddDoubleLine(L["DataText"].Death, Deaths, 1, 1, 1)
-			GameTooltip:AddDoubleLine(L["DataText"].Honor, string_format("%d", HonorGained), 1, 1, 1)
-			GameTooltip:AddDoubleLine(L["DataText"].Damage, K.ShortValue(DamageDone), 1, 1, 1)
-			GameTooltip:AddDoubleLine(L["DataText"].Healing, K.ShortValue(HealingDone), 1, 1, 1)
+			GameTooltip:AddDoubleLine(KILLING_BLOWS, KillingBlows, 1, 1, 1)
+			GameTooltip:AddDoubleLine(HONORABLE_KILLS, HonorableKills, 1, 1, 1)
+			GameTooltip:AddDoubleLine(DEATHS, Deaths, 1, 1, 1)
+			GameTooltip:AddDoubleLine(HONOR, string_format("%d", HonorGained), 1, 1, 1)
+			GameTooltip:AddDoubleLine(DAMAGE, K.ShortValue(DamageDone), 1, 1, 1)
+			GameTooltip:AddDoubleLine(HEALS, K.ShortValue(HealingDone), 1, 1, 1)
 
-			-- Add extra statistics based on what BG you're in.
-			if (CurMapID == WSG or CurMapID == TP) then
-				GameTooltip:AddDoubleLine(L["DataText"].FlagCapture, GetBattlefieldStatData(i, 1), 1, 1, 1)
-				GameTooltip:AddDoubleLine(L["DataText"].FlagReturn, GetBattlefieldStatData(i, 2), 1, 1, 1)
-			elseif (CurMapID == EOTS) then
-				GameTooltip:AddDoubleLine(L["DataText"].FlagCapture, GetBattlefieldStatData(i, 1), 1, 1, 1)
-			elseif (CurMapID == AV) then
-				GameTooltip:AddDoubleLine(L["DataText"].GraveyardAssault, GetBattlefieldStatData(i, 1), 1, 1, 1)
-				GameTooltip:AddDoubleLine(L["DataText"].GraveyardDefend, GetBattlefieldStatData(i, 2), 1, 1, 1)
-				GameTooltip:AddDoubleLine(L["DataText"].TowerAssault, GetBattlefieldStatData(i, 3), 1, 1, 1)
-				GameTooltip:AddDoubleLine(L["DataText"].TowerDefend, GetBattlefieldStatData(i, 4), 1, 1, 1)
-			elseif (CurMapID == SOTA) then
-				GameTooltip:AddDoubleLine(L["DataText"].DemolisherDestroy, GetBattlefieldStatData(i, 1), 1, 1, 1)
-				GameTooltip:AddDoubleLine(L["DataText"].GateDestroy, GetBattlefieldStatData(i, 2), 1, 1, 1)
-			elseif (CurMapID == IOC or CurMapID == TBFG or CurMapID == AB or CurMapID == ASH) then
-				GameTooltip:AddDoubleLine(L["DataText"].BaseAssault, GetBattlefieldStatData(i, 1), 1, 1, 1)
-				GameTooltip:AddDoubleLine(L["DataText"].BaseDefend, GetBattlefieldStatData(i, 2), 1, 1, 1)
-			elseif (CurrentMapID == TOK) then
-				GameTooltip:AddDoubleLine(L["DataText"].OrbPossession, GetBattlefieldStatData(i, 1), 1, 1, 1)
-				GameTooltip:AddDoubleLine(L["DataText"].VictoryPts, GetBattlefieldStatData(i, 2), 1, 1, 1)
-			elseif (CurrentMapID == SSM) then
-				GameTooltip:AddDoubleLine(L["DataText"].CartControl, GetBattlefieldStatData(i, 1), 1, 1, 1)
-			elseif (CurrentMapID == DG) then
-				GameTooltip:AddDoubleLine(L["DataText"].CartControl, GetBattlefieldStatData(i, 1), 1, 1, 1)
-				GameTooltip:AddDoubleLine(L["DataText"].BaseAssault, GetBattlefieldStatData(i, 1), 1, 1, 1)
-				GameTooltip:AddDoubleLine(L["DataText"].BaseDefend, GetBattlefieldStatData(i, 2), 1, 1, 1)
+			for j = 1, NumExtraStats do
+				GameTooltip:AddDoubleLine(GetBattlefieldStatInfo(j), GetBattlefieldStatData(i, j), 1,1,1)
 			end
+
 			break
 		end
 	end
+
 	GameTooltip:Show()
 end
 
-local function OnLeave()
-	if not GameTooltip:IsForbidden() then
-		GameTooltip:Hide() -- WHY??? BECAUSE FUCK GAMETOOLTIP, THATS WHY!!
-	end
+function Module:OnLeave()
+	GameTooltip:Hide()
 end
 
-local int = 2
-local function OnUpdate(_, t)
+function Module:OnUpdate(t)
 	int = int - t
 
 	if (int < 0) then
 		local Amount
-		RequestBattlefieldScoreData()
 		local NumScores = GetNumBattlefieldScores()
+
+		RequestBattlefieldScoreData()
 
 		for i = 1, NumScores do
 			local Name, KillingBlows, _, _, HonorGained, _, _, _, _, DamageDone, HealingDone = GetBattlefieldScore(i)
 
 			if (HealingDone > DamageDone) then
-				Amount = (NameColor..L["DataText"].Healing.."|r"..ValueColor..K.ShortValue(HealingDone).."|r")
+				Amount = (Module.NameColor..L["DataText"].Healing.."|r"..Module.ValueColor..K.ShortValue(HealingDone).."|r")
 			else
-				Amount = (NameColor..L["DataText"].Damage.."|r"..ValueColor..K.ShortValue(DamageDone).."|r")
+				Amount = (Module.NameColor..L["DataText"].Damage.."|r"..Module.ValueColor..K.ShortValue(DamageDone).."|r")
 			end
 
 			if (Name and Name == K.Name) then
-				DataTextBG.Text1:SetText(Amount)
-				DataTextBG.Text2:SetText(NameColor..L["DataText"].Honor.."|r"..ValueColor..string_format("%d", HonorGained).."|r")
-				DataTextBG.Text3:SetText(NameColor..L["DataText"].KillingBlow.."|r"..ValueColor..KillingBlows.."|r")
+				self.Text1:SetText(Amount)
+				self.Text2:SetText(Module.NameColor..L["DataText"].Honor.."|r"..Module.ValueColor..string_format("%d", HonorGained).."|r")
+				self.Text3:SetText(Module.NameColor..L["DataText"].KillingBlow.."|r"..Module.ValueColor..KillingBlows.."|r")
 			end
 		end
 
@@ -132,42 +91,54 @@ local function OnUpdate(_, t)
 	end
 end
 
-local function OnEvent(self, event)
-	if event == "PLAYER_ENTERING_WORLD" then
-		local InInstance, InstanceType = IsInInstance()
+function Module:OnEvent()
+	local InInstance, InstanceType = IsInInstance()
 
-		if (InInstance and (InstanceType == "pvp")) then
-			self:Show()
-		else
-			self:Hide()
-			DataTextBG.Text1:SetText("")
-			DataTextBG.Text2:SetText("")
-			DataTextBG.Text3:SetText("")
-		end
+	if (InInstance and (InstanceType == "pvp")) then
+		self:Show()
+	else
+		self:Hide()
+		self.Text1:SetText("")
+		self.Text2:SetText("")
+		self.Text3:SetText("")
 	end
 end
 
-DataTextBG.Text1 = K.SetFontString(DataTextBG, C["Media"].Font, C["Media"].FontSize, C["DataText"].Outline and "OUTLINE" or "", "LEFT")
-DataTextBG.Text1:SetShadowOffset(C["DataText"].Outline and 0 or 1.25, C["DataText"].Outline and -0 or -1.25)
-DataTextBG.Text1:SetPoint("LEFT", 5, 0)
-DataTextBG.Text1:SetHeight(DataTextBG:GetHeight())
-DataTextBG.Text1 = DataTextBG.Text1
+function Module:OnEnable()
+	if not (C["DataText"].Battleground) then
+		return
+	end
 
-DataTextBG.Text2 = K.SetFontString(DataTextBG, C["Media"].Font, C["Media"].FontSize, C["DataText"].Outline and "OUTLINE" or "")
-DataTextBG.Text2:SetShadowOffset(C["DataText"].Outline and 0 or 1.25, C["DataText"].Outline and -0 or -1.25)
-DataTextBG.Text2:SetPoint("LEFT", DataTextBG.Text1, "RIGHT", 5, 0)
-DataTextBG.Text2:SetHeight(DataTextBG:GetHeight())
-DataTextBG.Text2 = DataTextBG.Text2
+	local DataText_Font = K.GetFont(C["DataText"].Font)
 
-DataTextBG.Text3 = K.SetFontString(DataTextBG, C["Media"].Font, C["Media"].FontSize, C["DataText"].Outline and "OUTLINE" or "")
-DataTextBG.Text3:SetShadowOffset(C["DataText"].Outline and 0 or 1.25, C["DataText"].Outline and -0 or -1.25)
-DataTextBG.Text3:SetPoint("LEFT", DataTextBG.Text2, "RIGHT", 5, 0)
-DataTextBG.Text3:SetHeight(DataTextBG:GetHeight())
-DataTextBG.Text3 = DataTextBG.Text3
+	Module:SetSize(300, 13)
+	Module:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 0, -4)
+	Module:SetFrameLevel(4)
+	Module:SetFrameStrata("BACKGROUND")
 
-DataTextBG:RegisterEvent("PLAYER_ENTERING_WORLD")
-DataTextBG:SetScript("OnUpdate", OnUpdate)
-DataTextBG:SetScript("OnEvent", OnEvent)
-DataTextBG:SetScript("OnEnter", OnEnter)
-DataTextBG:SetScript("OnLeave", OnLeave)
-OnUpdate(DataTextBG, 2)
+	local Text1 = Module:CreateFontString(nil, "OVERLAY")
+	Text1:SetFontObject(DataText_Font)
+	Text1:SetPoint("LEFT", 5, 0)
+	Text1:SetHeight(Module:GetHeight())
+	Module.Text1 = Text1
+
+	local Text2 = Module:CreateFontString(nil, "OVERLAY")
+	Text2:SetFontObject(DataText_Font)
+	Text2:SetPoint("LEFT", Module.Text1, "RIGHT", 5, 0)
+	Text2:SetHeight(Module:GetHeight())
+	Module.Text2 = Text2
+
+	local Text3 = Module:CreateFontString(nil, "OVERLAY")
+	Text3:SetFontObject(DataText_Font)
+	Text3:SetPoint("LEFT", Module.Text2, "RIGHT", 5, 0)
+	Text3:SetHeight(Module:GetHeight())
+	Module.Text3 = Text3
+
+	Module:RegisterEvent("PLAYER_ENTERING_WORLD")
+	Module:SetScript("OnUpdate", Module.OnUpdate)
+	Module:SetScript("OnEvent", Module.OnEvent)
+	Module:SetScript("OnEnter", Module.OnEnter)
+	Module:SetScript("OnLeave", Module.OnLeave)
+end
+
+Module:OnEnable()
