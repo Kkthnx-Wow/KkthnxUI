68df69a82dd0c77a5539c212d0b18a06c38e644e
Josh <JRussell20@gmail.com>
2019-04-12 04:17:26 -0400

    Fix issue #38
    This resulted in leftover settings from the old/reverted scaling system.

--- KkthnxUI/Core/Kill.lua
+++ KkthnxUI/Core/Kill.lua
@@ -57,7 +57,7 @@ UnloadBlizzardFrames:SetScript("OnEvent", function()
 		end
 	end
 
-	if C["General"]["Scaling"].Value then
+	if C["General"].AutoScale then
 		Advanced_UseUIScale:Kill()
 		Advanced_UIScaleSlider:Kill()
 	end
