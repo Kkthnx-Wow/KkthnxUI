ec664cf4902b1066be58d9ce0c3ebb06db9f5a7c
Josh <JRussell20@gmail.com>
2019-07-12 06:46:34 -0400

    Update conflicts file.

--- KkthnxUI/Core/Conflicts.lua
+++ KkthnxUI/Core/Conflicts.lua
@@ -1,14 +1,16 @@
-local K, C = unpack(select(2, ...))
+local _, C = unpack(select(2, ...))
 
--- Prevent users config errors and using other UIs over mine.
+-- Prevent Users Config Errors And Using Other UIs Over Mine.
 
 local _G = _G
 
 local DisableAddOn = _G.DisableAddOn
 local ReloadUI = _G.ReloadUI
 local IsAddOnLoaded = _G.IsAddOnLoaded
+local StaticPopupDialogs = _G.StaticPopupDialogs
+local StaticPopup_Show = _G.StaticPopup_Show
 
--- Force user to disable KkthnxUI if another AddOn is enabled with it!
+-- Force User To Disable Kkthnxui If Another Addon Is Enabled With It!
 if IsAddOnLoaded("KkthnxUI") and IsAddOnLoaded("Tukui") or IsAddOnLoaded("ElvUI") or IsAddOnLoaded("DiabolicUI") or IsAddOnLoaded("DuffedUI") or IsAddOnLoaded("ShestakUI") then
 	StaticPopupDialogs.KKTHNXUI_INCOMPATIBLE = {
 		text = "Oh no, you have |cff4488ffKkthnxUI|r and another UserInterface enabled at the same time. Disable KkthnxUI!",
@@ -35,117 +37,103 @@ if IsAddOnLoaded("KkthnxUI") and IsAddOnLoaded("Tukui") or IsAddOnLoaded("ElvUI"
 	return
 end
 
--- Auto-overwrite script config is X addon is found. Here we use our own functions to check for addons.
+-- Auto-Overwrite Script Config Is X Addon Is Found. Here We Use Our Own Functions To Check For Addons.
 if C["DataBars"].Enable == false then
 	C["DataBars"].TrackHonor = false
 end
 
-if K.CheckAddOnState("flyPlateBuffs") then
+if IsAddOnLoaded("flyPlateBuffs") then
 	C["Nameplates"].TrackAuras = false
 end
 
-if K.CheckAddOnState("SexyMap")
-or K.CheckAddOnState("bdMinimap")
-or K.CheckAddOnState("BasicMinimap")
-or K.CheckAddOnState("RicoMiniMap")
-or K.CheckAddOnState("Chinchilla") then
+if IsAddOnLoaded("SexyMap")
+or IsAddOnLoaded("bdMinimap")
+or IsAddOnLoaded("BasicMinimap")
+or IsAddOnLoaded("RicoMiniMap")
+or IsAddOnLoaded("Chinchilla") then
 	C["Minimap"].Enable = false
 end
 
-if K.CheckAddOnState("XPerl")
-or K.CheckAddOnState("Stuf")
-or K.CheckAddOnState("PitBull4")
-or K.CheckAddOnState("ShadowedUnitFrames") then
+if IsAddOnLoaded("XPerl")
+or IsAddOnLoaded("Stuf")
+or IsAddOnLoaded("PitBull4")
+or IsAddOnLoaded("ShadowedUnitFrames") then
 	C["Unitframe"].Enable = false
 	C["Party"].Enable = false
 	C["Arena"].Enable = false
 	C["Boss"].Enable = false
 end
 
-if K.CheckAddOnState("Dominos")
-or K.CheckAddOnState("Bartender4")
-or K.CheckAddOnState("RazerNaga")
-or K.CheckAddOnState("daftMainBar")
-or (K.CheckAddOnState("ConsolePortBar") and K.CheckAddOnState("ConsolePort")) then -- We have to check for main ConsolePort addon too.
+if IsAddOnLoaded("Dominos")
+or IsAddOnLoaded("Bartender4")
+or IsAddOnLoaded("RazerNaga")
+or IsAddOnLoaded("daftMainBar")
+or (IsAddOnLoaded("ConsolePortBar") and IsAddOnLoaded("ConsolePort")) then -- We Have To Check For Main Consoleport Addon Too.
+	C["DataBars"].Enable = false
 	C["ActionBar"].Enable = false
 	C["ActionBar"].MicroBar = false
 	C["BagBar"].Enable = false
 end
 
-if K.CheckAddOnState("Dominos")
-or K.CheckAddOnState("Bartender4")
-or K.CheckAddOnState("daftMainBar") then -- We have to check for main ConsolePort addon too.
-	C["ActionBar"].Enable = false
-	C["ActionBar"].MicroBar = false
-	C["BagBar"].Enable = false
-end
-
-if K.CheckAddOnState("Mapster")
-or K.CheckAddOnState("WorldQuestsList") then
+if IsAddOnLoaded("Mapster")
+or IsAddOnLoaded("WorldQuestsList") then
 	C["WorldMap"].SmallWorldMap = false
 end
 
-if K.CheckAddOnState("AdiBags")
-or K.CheckAddOnState("ArkInventory")
-or K.CheckAddOnState("cargBags_Nivaya")
-or K.CheckAddOnState("cargBags")
-or K.CheckAddOnState("Bagnon")
-or K.CheckAddOnState("Combuctor")
-or K.CheckAddOnState("TBag")
-or K.CheckAddOnState("BaudBag") then
+if IsAddOnLoaded("AdiBags")
+or IsAddOnLoaded("ArkInventory")
+or IsAddOnLoaded("cargBags_Nivaya")
+or IsAddOnLoaded("cargBags")
+or IsAddOnLoaded("Bagnon")
+or IsAddOnLoaded("Combuctor")
+or IsAddOnLoaded("TBag")
+or IsAddOnLoaded("BaudBag") then
 	C["Inventory"].Enable = false
 end
 
-if K.CheckAddOnState("Prat-3.0")
-or K.CheckAddOnState("Chatter") then
+if IsAddOnLoaded("Prat-3.0")
+or IsAddOnLoaded("Chatter") then
 	C["Chat"].Enable = false
 end
 
-if K.CheckAddOnState("TidyPlates")
-or K.CheckAddOnState("Aloft")
-or K.CheckAddOnState("Kui_Nameplates")
-or K.CheckAddOnState("bdNameplates")
-or K.CheckAddOnState("NiceNameplates") then
+if IsAddOnLoaded("TidyPlates")
+or IsAddOnLoaded("Aloft")
+or IsAddOnLoaded("Kui_Nameplates")
+or IsAddOnLoaded("bdNameplates")
+or IsAddOnLoaded("Plater")
+or IsAddOnLoaded("NiceNameplates") then
 	C["Nameplates"].Enable = false
 end
 
-if K.CheckAddOnState("TipTop")
-or K.CheckAddOnState("TipTac")
-or K.CheckAddOnState("FreebTip")
-or K.CheckAddOnState("bTooltip")
-or K.CheckAddOnState("PhoenixTooltip")
-or K.CheckAddOnState("Icetip")
-or K.CheckAddOnState("rTooltip") then
+if IsAddOnLoaded("TipTop")
+or IsAddOnLoaded("TipTac")
+or IsAddOnLoaded("FreebTip")
+or IsAddOnLoaded("bTooltip")
+or IsAddOnLoaded("PhoenixTooltip")
+or IsAddOnLoaded("Icetip")
+or IsAddOnLoaded("rTooltip") then
 	C["Tooltip"].Enable = false
 end
 
-if K.CheckAddOnState("TipTacTalents") then
+if IsAddOnLoaded("TipTacTalents") then
 	C["Tooltip"].Talents = false
 end
 
-if K.CheckAddOnState("ConsolePortBar") then
-	C["DataBars"].Enable = false
-end
-
-if K.CheckAddOnState("cInterrupt") then
-	C["Announcements"].Interrupt = false
-end
-
-if K.CheckAddOnState("NiceBubbles") then
+if IsAddOnLoaded("NiceBubbles") then
 	C["Skins"].ChatBubble = false
 end
 
-if K.CheckAddOnState("ChatSounds") then
+if IsAddOnLoaded("ChatSounds") then
 	C["Chat"].WhispSound = false
 end
 
-if K.CheckAddOnState("MBB")
-or K.CheckAddOnState("MinimapButtonFrame") then
+if IsAddOnLoaded("MBB")
+or IsAddOnLoaded("MinimapButtonFrame") then
 	C["Minimap"].CollectButtons = false
 end
 
-if K.CheckAddOnState("OmniCC")
-or K.CheckAddOnState("ncCooldown")
-or K.CheckAddOnState("CooldownCount") then
+if IsAddOnLoaded("OmniCC")
+or IsAddOnLoaded("ncCooldown")
+or IsAddOnLoaded("CooldownCount") then
 	C["ActionBar"].Cooldowns = false
 end
