1c978429445d424cf9d3906d917a4c2c359e9fa9
Kkthnx <40672673+Kkthnx@users.noreply.github.com>
2023-01-22 04:28:26 -0500

    Update Sapped.lua
    Do not hard code like that :-1:

--- KkthnxUI/Modules/Announcements/Elements/Sapped.lua
+++ KkthnxUI/Modules/Announcements/Elements/Sapped.lua
@@ -10,10 +10,7 @@ local UNKNOWN = _G.UNKNOWN
 -- SetupSaySapped() checks for when Sap is applied or refreshed on the player, and then sends a chat message and prints a message in the chat window.
 local function SetupSaySapped()
 	-- Get combat log info for the current event.
-	local _, event, _, _, sourceName, _, _, _, destName = CombatLogGetCurrentEventInfo()
-
-	-- Set spellID to Sap (6770).
-	local spellID = 6770
+	local _, event, _, _, sourceName, _, _, _, destName, spellID = CombatLogGetCurrentEventInfo()
 
 	-- If the spellID is Sap (6770), the destination name is the player's name and the event type is either "SPELL_AURA_APPLIED" or "SPELL_AURA_REFRESH", then execute this code.
 	if (spellID == 6770) and (destName == K.Name) and (event == "SPELL_AURA_APPLIED" or event == "SPELL_AURA_REFRESH") then
