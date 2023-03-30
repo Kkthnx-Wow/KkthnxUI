41e59f8e918f665b1c2df5bc5e47377fd428c760
Kkthnx <40672673+Kkthnx@users.noreply.github.com>
2023-03-29 20:20:31 -0400

    these changes should enhance the usability and maintainability of the pet action bar module.

--- KkthnxUI/Modules/ActionBars/Elements/Petbar.lua
+++ KkthnxUI/Modules/ActionBars/Elements/Petbar.lua
@@ -1,4 +1,4 @@
-local K, C, L = KkthnxUI[1], KkthnxUI[2], KkthnxUI[3]
+local K = KkthnxUI[1]
 local Module = K:GetModule("ActionBar")
 
 -- Cached globals
@@ -11,21 +11,26 @@ local NUM_PET_ACTION_SLOTS = NUM_PET_ACTION_SLOTS
 local PET_ACTION_HIGHLIGHT_MARKS = PET_ACTION_HIGHLIGHT_MARKS
 local SharedActionButton_RefreshSpellHighlight = SharedActionButton_RefreshSpellHighlight
 
-local tinsert = tinsert
 local margin = 6
 
 local function hasPetActionHighlightMark(index)
 	return PET_ACTION_HIGHLIGHT_MARKS[index]
 end
 
+-- Update pet action buttons
 function Module:UpdatePetBar()
 	local petActionButton, petActionIcon, petAutoCastableTexture, petAutoCastShine
 	for i = 1, NUM_PET_ACTION_SLOTS, 1 do
+		-- Get the action button and its subcomponents
 		petActionButton = self.actionButtons[i]
 		petActionIcon = petActionButton.icon
 		petAutoCastableTexture = petActionButton.AutoCastable
 		petAutoCastShine = petActionButton.AutoCastShine
+
+		-- Get info about the pet action
 		local name, texture, isToken, isActive, autoCastAllowed, autoCastEnabled, spellID = GetPetActionInfo(i)
+
+		-- Set icon and tooltip for pet action
 		if not isToken then
 			petActionIcon:SetTexture(texture)
 			petActionButton.tooltipName = name
@@ -33,13 +38,19 @@ function Module:UpdatePetBar()
 			petActionIcon:SetTexture(_G[texture])
 			petActionButton.tooltipName = _G[name]
 		end
+
+		-- Set pet action button data
 		petActionButton.isToken = isToken
+
+		-- Load spell data and set tooltip subtext
 		if spellID then
 			local spell = Spell:CreateFromSpellID(spellID)
 			petActionButton.spellDataLoadedCancelFunc = spell:ContinueWithCancelOnSpellLoad(function()
 				petActionButton.tooltipSubtext = spell:GetSpellSubtext()
 			end)
 		end
+
+		-- Set button visual state based on whether it is active or not
 		if isActive then
 			if IsPetAttackAction(i) then
 				petActionButton:StartFlash()
@@ -54,16 +65,22 @@ function Module:UpdatePetBar()
 			petActionButton:StopFlash()
 			petActionButton:SetChecked(false)
 		end
+
+		-- Show or hide the auto-castable texture
 		if autoCastAllowed then
 			petAutoCastableTexture:Show()
 		else
 			petAutoCastableTexture:Hide()
 		end
+
+		-- Start or stop the auto-cast shine animation
 		if autoCastEnabled then
 			AutoCastShine_AutoCastStart(petAutoCastShine)
 		else
 			AutoCastShine_AutoCastStop(petAutoCastShine)
 		end
+
+		-- Set icon color based on whether the action is usable or not
 		if texture then
 			if GetPetActionSlotUsable(i) then
 				petActionIcon:SetVertexColor(1, 1, 1)
@@ -75,46 +92,67 @@ function Module:UpdatePetBar()
 			petActionIcon:Hide()
 		end
 
+		-- Refresh spell highlight
 		SharedActionButton_RefreshSpellHighlight(petActionButton, hasPetActionHighlightMark(i))
 	end
+
+	-- Update cooldowns and range timer
 	self:UpdateCooldowns()
 	self.rangeTimer = -1
 end
 
+-- Function to handle events that update the pet action bar
 function Module.PetBarOnEvent(event)
+	-- If the event is PET_BAR_UPDATE_COOLDOWN, call UpdateCooldowns for the pet action bar
 	if event == "PET_BAR_UPDATE_COOLDOWN" then
 		PetActionBar:UpdateCooldowns()
+	-- Otherwise, call UpdatePetBar for the pet action bar
 	else
 		Module.UpdatePetBar(PetActionBar)
 	end
 end
 
-function Module:CreatePetbar()
-	local num = NUM_PET_ACTION_SLOTS
-	local buttonList = {}
+-- Create pet action bar
+function Module:CreatePetActionBar()
+	-- Number of pet action slots
+	local numPetActions = NUM_PET_ACTION_SLOTS
+	-- Empty table to hold pet action buttons
+	local petActionButtons = {}
 
+	-- Create the frame for the pet action bar
 	local frame = CreateFrame("Frame", "KKUI_ActionBarPet", UIParent, "SecureHandlerStateTemplate")
+	-- Set the mover for the frame
 	frame.mover = K.Mover(frame, "Pet Actionbar", "PetBar", { "BOTTOM", _G.KKUI_ActionBar3, "TOP", 0, margin })
+	-- Add the mover to the movers list
 	Module.movers[10] = frame.mover
 
-	for i = 1, num do
-		local button = _G["PetActionButton" .. i]
-		button:SetParent(frame)
-		tinsert(buttonList, button)
-		tinsert(Module.buttons, button)
+	-- Loop through each pet action slot
+	for i = 1, numPetActions do
+		-- Get the pet action button
+		local petActionButton = _G["PetActionButton" .. i]
+		-- Set the parent of the pet action button to the frame
+		petActionButton:SetParent(frame)
+		-- Add the pet action button to the list of pet action buttons
+		table.insert(petActionButtons, petActionButton)
+		-- Add the pet action button to the list of all action buttons
+		table.insert(Module.buttons, petActionButton)
 
-		local hotkey = button.HotKey
+		-- Set the position of the hotkey text
+		local hotkey = petActionButton.HotKey
 		if hotkey then
 			hotkey:ClearAllPoints()
 			hotkey:SetPoint("TOPRIGHT", 0, -2)
 		end
 	end
-	frame.buttons = buttonList
 
+	-- Set the list of pet action buttons for the frame
+	frame.buttons = petActionButtons
+
+	-- Set the visibility state for the frame
 	frame.frameVisibility = "[petbattle][overridebar][vehicleui][possessbar][shapeshift] hide; [pet] show; hide"
 	RegisterStateDriver(frame, "visibility", frame.frameVisibility)
 
-	-- Fix pet bar updating
+	-- Set the events that will trigger updates to the pet action bar
 	local events = {
 		"UNIT_PET",
 		"UNIT_FLAGS",
@@ -130,7 +168,10 @@ function Module:CreatePetbar()
 		"PLAYER_FARSIGHT_FOCUS_CHANGED",
 	}
 
+	-- Call the PetBarOnEvent function to update the pet action bar
 	Module:PetBarOnEvent()
+
+	-- Register each event with the PetBarOnEvent function
 	for _, event in ipairs(events) do
 		K:RegisterEvent(event, Module.PetBarOnEvent)
 	end
--- KkthnxUI/Modules/ActionBars/Elements/Stancebar.lua
+++ KkthnxUI/Modules/ActionBars/Elements/Stancebar.lua
@@ -56,23 +56,36 @@ function Module:UpdateStanceBar()
 	frame.mover:SetSize(size, size)
 end
 
+-- Updates the stance bar
 function Module:UpdateStance()
+	-- Check if the player is in combat
 	local inCombat = InCombatLockdown()
+
+	-- Get the number of available shapeshift forms
 	local numForms = GetNumShapeshiftForms()
+
+	-- Declare variables to be used later
 	local texture, isActive, isCastable
 	local icon, cooldown
 	local start, duration, enable
 
+	-- Loop through all the action buttons
 	for i, button in pairs(self.actionButtons) do
+		-- If not in combat, hide the button
 		if not inCombat then
 			button:Hide()
 		end
+
+		-- Get the button icon
 		icon = button.icon
+
+		-- Check if the button corresponds to a valid shapeshift form
 		if i <= numForms then
+			-- Get information about the shapeshift form
 			texture, isActive, isCastable = GetShapeshiftFormInfo(i)
 			icon:SetTexture(texture)
 
-			--Cooldown stuffs
+			-- Show/hide the button and its cooldown depending on whether the form has a texture and the player is not in combat
 			cooldown = button.cooldown
 			if texture then
 				if not inCombat then
@@ -82,30 +95,33 @@ function Module:UpdateStance()
 			else
 				cooldown:Hide()
 			end
+
+			-- Set the cooldown on the button
 			start, duration, enable = GetShapeshiftFormCooldown(i)
 			CooldownFrame_Set(cooldown, start, duration, enable)
 
-			if isActive then
-				button:SetChecked(true)
-			else
-				button:SetChecked(false)
-			end
+			-- Set the button's checked status depending on whether the form is active
+			button:SetChecked(isActive)
 
+			-- Set the color of the button icon depending on whether the form is castable
 			if isCastable then
-				icon:SetVertexColor(1.0, 1.0, 1.0)
+				icon:SetVertexColor(1.0, 1.0, 1.0) -- white
 			else
-				icon:SetVertexColor(0.4, 0.4, 0.4)
+				icon:SetVertexColor(0.4, 0.4, 0.4) -- gray
 			end
 		end
 	end
 end
 
+-- Called when the event fires for updating the stance bar
 function Module:StanceBarOnEvent()
-	Module:UpdateStanceBar()
-	Module.UpdateStance(StanceBar)
+	Module:UpdateStanceBar() -- Update the stance bar
+	Module.UpdateStance(StanceBar) -- Update the stance
 end
 
-function Module:CreateStancebar()
+-- Creates the stance bar
+function Module:CreateStanceBar()
+	-- Return if the stance bar is not enabled in the config
 	if not C["ActionBar"].ShowStance then
 		return
 	end
@@ -115,12 +131,9 @@ function Module:CreateStancebar()
 	frame.mover = K.Mover(frame, "StanceBar", "StanceBar", { "BOTTOMLEFT", _G.KKUI_ActionBar3, "TOPLEFT", 0, margin })
 	Module.movers[11] = frame.mover
 
-	-- StanceBar
-	StanceBar:SetParent(frame)
-	StanceBar:EnableMouse(false)
-	StanceBar:UnregisterAllEvents()
-
-	for i = 1, num do
+	-- Stance buttons
+	local numForms = GetNumShapeshiftForms()
+	for i = 1, numForms do
 		local button = _G["StanceButton" .. i]
 		button:SetParent(frame)
 		tinsert(buttonList, button)
@@ -128,12 +141,21 @@ function Module:CreateStancebar()
 	end
 	frame.buttons = buttonList
 
-	-- Fix stance bar updating
-	Module:StanceBarOnEvent()
+	-- Set the parent of the stance bar to the new frame
+	StanceBar:SetParent(frame)
+
+	-- Disable mouse interaction with the stance bar
+	StanceBar:EnableMouse(false)
+
+	-- Unregister all events from the default stance bar
+	StanceBar:UnregisterAllEvents()
+
+	-- Register events for updating the stance bar
 	K:RegisterEvent("UPDATE_SHAPESHIFT_FORMS", Module.StanceBarOnEvent)
 	K:RegisterEvent("UPDATE_SHAPESHIFT_USABLE", Module.StanceBarOnEvent)
 	K:RegisterEvent("UPDATE_SHAPESHIFT_COOLDOWN", Module.StanceBarOnEvent)
 
+	-- Set up the frame visibility state driver
 	frame.frameVisibility = "[petbattle][overridebar][vehicleui][possessbar,@vehicle,exists][shapeshift] hide; show"
 	RegisterStateDriver(frame, "visibility", frame.frameVisibility)
 end
