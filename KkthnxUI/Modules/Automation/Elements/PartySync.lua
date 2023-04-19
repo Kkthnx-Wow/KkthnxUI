05b641b0b8c3bb7fcc755015f753e9aef1ce9b2e
Kkthnx <40672673+Kkthnx@users.noreply.github.com>
2023-04-19 18:01:13 -0400

    Code updates for partysync and whisperinv

--- KkthnxUI/Modules/Automation/Elements/PartySync.lua
+++ KkthnxUI/Modules/Automation/Elements/PartySync.lua
@@ -4,55 +4,45 @@ local Module = K:GetModule("Automation")
 -- Sourced: Leatrix Plus (Leatrix)
 -- Edited: KkthnxUI (Kkthnx)
 
-local string_split = string.split
-
-local BNET_CLIENT_WOW = BNET_CLIENT_WOW
+-- Import necessary functions and modules
 local BNGetNumFriends = BNGetNumFriends
 local C_BattleNet_GetFriendGameAccountInfo = C_BattleNet.GetFriendGameAccountInfo
 local C_BattleNet_GetFriendNumGameAccounts = C_BattleNet.GetFriendNumGameAccounts
-local C_FriendList_GetFriendInfoByIndex = C_FriendList.GetFriendInfoByIndex
-local C_FriendList_GetNumFriends = C_FriendList.GetNumFriends
-local C_FriendList_ShowFriends = C_FriendList.ShowFriends
+local C_FriendList_IsFriend = C_FriendList.IsFriend
 local C_QuestSession_GetSessionBeginDetails = C_QuestSession.GetSessionBeginDetails
-local GetGuildRosterInfo = GetGuildRosterInfo
-local GetNumGuildMembers = GetNumGuildMembers
-local QuestSessionManager = QuestSessionManager
+local hooksecurefunc = hooksecurefunc
+local string_split = string.split
 local UnitGUID = UnitGUID
 local UnitName = UnitName
-local hooksecurefunc = hooksecurefunc
 
--- Cache frequently accessed global functions
-local string_split = string.split
-local C_QuestSession_GetSessionBeginDetails = C_QuestSession.GetSessionBeginDetails
-local C_FriendList_IsFriend = C_FriendList.IsFriend
+-- Define party unit IDs
+local PARTY_UNITS = { "player", "party1", "party2", "party3", "party4" }
 
-local partyUnitsList = { "player", "party1", "party2", "party3", "party4" }
-
--- Check if a name is in your friends list
-local function IsFriendCheck(friendName)
+-- Check if a given name is in your friends list
+local function isFriend(name)
 	-- Do nothing if name is empty (such as whispering from the Battle.net app)
-	if not friendName then
+	if not name then
 		return
 	end
 
 	-- Remove realm
-	friendName = string_split("-", friendName, 2)
+	name = string_split("-", name, 2)
 
 	-- Check character friends
-	if C_FriendList_IsFriend(friendName) then
+	if C_FriendList_IsFriend(name) then
 		return true
 	end
 
 	-- Check Battle.net friends
-	local numBNet = BNGetNumFriends()
-	for i = 1, numBNet do
+	local numBNetFriends = BNGetNumFriends()
+	for i = 1, numBNetFriends do
 		local numGameAccounts = C_BattleNet_GetFriendNumGameAccounts(i)
 		if numGameAccounts > 0 then
 			for j = 1, numGameAccounts do
 				local gameAccountInfo = C_BattleNet_GetFriendGameAccountInfo(i, j)
 				local charName = gameAccountInfo.characterName
 				local client = gameAccountInfo.clientProgram
-				if client == BNET_CLIENT_WOW and charName == friendName then
+				if client == "WoW" and charName == name then
 					return true
 				end
 			end
@@ -60,15 +50,17 @@ local function IsFriendCheck(friendName)
 	end
 end
 
-local function SetupAutoPartySyncAccept(self)
-	if C_QuestSession_GetSessionBeginDetails() then
-		for _, unit in ipairs(partyUnitsList) do
-			if UnitGUID(unit) == C_QuestSession_GetSessionBeginDetails().guid then
+-- Accept a Party Sync invitation if it's from a friend and auto-accept is enabled
+local function setupAutoPartySyncAccept(self)
+	local sessionBeginDetails = C_QuestSession_GetSessionBeginDetails()
+	if sessionBeginDetails then
+		for _, unit in ipairs(PARTY_UNITS) do
+			if UnitGUID(unit) == sessionBeginDetails.guid then
 				local requesterName = UnitName(unit)
-				if requesterName and IsFriendCheck(requesterName) then
+				if requesterName and isFriend(requesterName) then
 					self.ButtonContainer.Confirm:Click()
 					K.Print("You have auto accepted a Party Sync from " .. requesterName)
-					K.Print("If you do not want to auto accept these. You can turn it off in KkthnxUI Config")
+					K.Print("If you do not want to auto accept these, you can turn it off in KkthnxUI Config")
 				end
 				return
 			end
@@ -76,10 +68,11 @@ local function SetupAutoPartySyncAccept(self)
 	end
 end
 
+-- Create a hook to automatically accept Party Sync invitations
 function Module:CreateAutoPartySyncAccept()
 	if not C["Automation"].AutoPartySync then
 		return
 	end
 
-	hooksecurefunc(QuestSessionManager.StartDialog, "Show", SetupAutoPartySyncAccept)
+	hooksecurefunc(QuestSessionManager.StartDialog, "Show", setupAutoPartySyncAccept)
 end
--- KkthnxUI/Modules/Automation/Elements/WhisperInvite.lua
+++ KkthnxUI/Modules/Automation/Elements/WhisperInvite.lua
@@ -1,74 +1,91 @@
 local K, C = KkthnxUI[1], KkthnxUI[2]
 local Module = K:GetModule("Automation")
 
-local string_lower = string.lower
-local IsInGroup = IsInGroup
-local UnitIsGroupAssistant = UnitIsGroupAssistant
-local UnitIsGroupLeader = UnitIsGroupLeader
+-- Auto-invite players to group when they whisper a specified keyword,
+-- and optionally when they're in the same guild.
 
-local BNInviteFriend = BNInviteFriend
+local C_PartyInfo_InviteUnit = C_PartyInfo.InviteUnit
 local C_BattleNet_GetAccountInfoByID = C_BattleNet.GetAccountInfoByID
-local CanCooperateWithGameAccount = CanCooperateWithGameAccount
+local C_GuildInfo_GuildRoster = C_GuildInfo.GuildRoster
 
-local isGuildMember = {}
-local isGroupLeaderOrAssistant = false
+-- Create an empty table to hold the list of whisper keywords
+local whisperList = {}
 
--- Check if the player is in a group and is the leader or assistant
-local function checkGroupStatus()
-	isGroupLeaderOrAssistant = IsInGroup() and (UnitIsGroupLeader("player") or UnitIsGroupAssistant("player"))
+-- Function to update the whisper keyword list
+local function updateWhisperList()
+	-- Clear the table
+	table.wipe(whisperList)
+
+	-- Get the list of keywords from the saved settings
+	local inviteKeyword = string.lower(C["Automation"].WhisperInvite)
+	if inviteKeyword and inviteKeyword ~= "" then
+		local keywords = { strsplit(",", inviteKeyword) }
+		for _, keyword in ipairs(keywords) do
+			-- Add each keyword to the table, converted to lowercase for case-insensitive matching
+			whisperList[string.lower(keyword)] = true
+		end
+	end
 end
 
--- Check if a unit name is in the guild roster
+-- Function to check if a unit is in the player's guild
 local function isUnitInGuild(unitName)
-	if isGuildMember[unitName] ~= nil then
-		return isGuildMember[unitName]
+	if not unitName then
+		return false
 	end
-
-	for i = 1, GetNumGuildMembers() do
-		local name = GetGuildRosterInfo(i)
-		if name and Ambiguate(name, "none") == Ambiguate(unitName, "none") then
-			isGuildMember[unitName] = true
+	-- Get the list of guild members
+	local guildMembers = C_GuildInfo_GuildRoster()
+	for _, memberInfo in ipairs(guildMembers) do
+		-- Compare the unit name to the guild member name, using Ambiguate to handle cross-realm names
+		local name = Ambiguate(memberInfo.name, "none")
+		if name == Ambiguate(unitName, "none") then
+			-- Return true if the unit is found in the guild
 			return true
 		end
 	end
-
-	isGuildMember[unitName] = false
+	-- Return false if the unit is not found in the guild
 	return false
 end
 
--- Handle whisper invite events
-local function handleWhisperInvite(event, msg, author, _, _, _, _, _, _, _, _, guid, presenceID)
-	local lowerMsg = string_lower(msg)
-	local lowerAuthor = string_lower(author)
-
-	-- Check if the message matches the whisper invite keyword and the player is in a group and is the leader or assistant
-	if lowerMsg == string_lower(C["Automation"].WhisperInvite) and isGroupLeaderOrAssistant then
-		if event == "CHAT_MSG_BN_WHISPER" then
-			local accountInfo = C_BattleNet_GetAccountInfoByID(presenceID)
-			if accountInfo then
-				local gameAccountInfo = accountInfo.gameAccountInfo
-				local gameID = gameAccountInfo.gameAccountID
-				if gameID then
-					local charName = gameAccountInfo.characterName
-					local realmName = gameAccountInfo.realmName
-					if CanCooperateWithGameAccount(accountInfo) and isUnitInGuild(charName .. "-" .. realmName) then
-						BNInviteFriend(gameID)
+-- Function to handle incoming whispers
+local function onChatWhisper(event, msg, author, _, _, _, _, _, _, _, _, presenceID)
+	-- Convert the message and author names to lowercase for case-insensitive matching
+	local lowerMsg = string.lower(msg)
+	for keyword in pairs(whisperList) do
+		-- Check if the message contains any of the keywords, and if the player is eligible to invite
+		if string.find(lowerMsg, keyword) and (not IsInGroup() or UnitIsGroupLeader("player") or UnitIsGroupAssistant("player")) then
+			if event == "CHAT_MSG_BN_WHISPER" then
+				-- If the whisper is from a Battle.net friend, get their account info
+				local accountInfo = C_BattleNet_GetAccountInfoByID(presenceID)
+				if accountInfo then
+					-- Get the game account info and ID from the Battle.net account info
+					local gameAccountInfo = accountInfo.gameAccountInfo
+					local gameID = gameAccountInfo.gameAccountID
+					if gameID then
+						-- Get the character name and realm name from the game account info
+						local charName = gameAccountInfo.characterName
+						local realmName = gameAccountInfo.realmName
+						-- Check if the player can cooperate with the friend's game account, and if the friend is in the player's guild
+						if CanCooperateWithGameAccount(accountInfo) and isUnitInGuild(charName .. "-" .. realmName) then
+							-- Invite the friend to the player's group
+							BNInviteFriend(gameID)
+						end
 					end
 				end
-			end
-		else
-			if isUnitInGuild(author) then
-				InviteToGroup(author)
+			else
+				-- If the whisper is from a player, check if they are in the player's guild
+				if isUnitInGuild(author) then
+					C_PartyInfo_InviteUnit(author)
+				end
 			end
 		end
 	end
 end
 
 function Module:CreateAutoWhisperInvite()
-	-- Check if the player is in a group and is the leader or assistant
-	checkGroupStatus()
-
-	-- Register events
-	K:RegisterEvent("CHAT_MSG_WHISPER", handleWhisperInvite)
-	K:RegisterEvent("CHAT_MSG_BN_WHISPER", handleWhisperInvite)
+	-- Update the list of keywords to trigger auto-invites
+	updateWhisperList()
+	-- Register the onChatWhisper function to handle incoming whispers
+	K:RegisterEvent("CHAT_MSG_WHISPER", onChatWhisper)
+	-- Register the onChatWhisper function to handle incoming Battle.net whispers
+	K:RegisterEvent("CHAT_MSG_BN_WHISPER", onChatWhisper)
 end
