32533d25b2d35308aee3ca22d4ea68bae5fa0f4f
Kkthnx <40672673+Kkthnx@users.noreply.github.com>
2023-05-25 23:35:09 -0400

    Update GroupLoot.lua
    Overlooked this. Edited on my phone 🤳

--- KkthnxUI/Modules/Loot/Elements/GroupLoot.lua
+++ KkthnxUI/Modules/Loot/Elements/GroupLoot.lua
@@ -248,14 +248,14 @@ function Module:LootFrame_GetFrame(i)
 	end
 end
 
-function Module:CANCEL_LOOT_ROLL(_, rollID)
+function Module.CANCEL_LOOT_ROLL(self, _, rollID)
 	if self.rollID == rollID then
 		self.rollID = nil
 		self.time = nil
 	end
 end
 
-function Module:START_LOOT_ROLL(event, rollID, rollTime)
+function Module.START_LOOT_ROLL(self, event, rollID, rollTime)
 	local texture, name, count, quality, bop, canNeed, canGreed, canDisenchant, _, _, _, _, canTransmog = GetLootRollItemInfo(rollID)
 	if not name then -- also done in GroupLootFrame_OnShow
 		for _, rollBar in next, Module.RollBars do
